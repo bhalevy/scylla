a38b2e5654c39b05dec8571c827d30137669f120
Benny Halevy <bhalevy@scylladb.com>
2019-09-12 10:18:22 +0300

    sstables: read_toc: always close file
    Use utils::with_file helper to always close the file
    new_sstable_component_file opens.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -65,6 +65,7 @@
 #include "binary_search.hh"
 #include "utils/bloom_filter.hh"
 #include "utils/memory_data_sink.hh"
+#include "utils/with_file.hh"
 
 #include "checked-file-impl.hh"
 #include "integrity_checked_file_impl.hh"
@@ -866,7 +867,7 @@ future<> sstable::read_toc() {
 
     sstlog.debug("Reading TOC file {} ", filename(component_type::TOC));
 
-    return new_sstable_component_file(_read_error_handler, component_type::TOC, open_flags::ro).then([this] (file f) {
+    return utils::with_file(new_sstable_component_file(_read_error_handler, component_type::TOC, open_flags::ro), [this] (file f) {
         auto bufptr = allocate_aligned_buffer<char>(4096, 4096);
         auto buf = bufptr.get();
 
@@ -899,7 +900,6 @@ future<> sstable::read_toc() {
             if (!_recognized_components.size()) {
                 throw malformed_sstable_exception("Empty TOC", filename(component_type::TOC));
             }
-            return f.close().finally([f] {});
         });
     }).then_wrapped([this] (future<> f) {
         try {
