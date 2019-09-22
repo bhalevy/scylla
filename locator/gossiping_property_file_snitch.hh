7fe775b7660d62bbe742939163780db9483b9dce
Benny Halevy <bhalevy@scylladb.com>
2019-09-22 13:35:45 +0300

    locator::gossiping_property_file_snitch: use file_stat
    Rather than opening the file (and not closing it).
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- locator/gossiping_property_file_snitch.hh
+++ locator/gossiping_property_file_snitch.hh
@@ -134,7 +134,7 @@ private:
 
 private:
     timer<lowres_clock> _file_reader;
-    std::optional<timespec> _last_file_mod;
+    std::optional<std::chrono::system_clock::time_point> _last_file_mod;
     std::istringstream _istrm;
     bool _file_reader_runs = false;
     unsigned _file_reader_cpu_id;
