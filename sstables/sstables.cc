3cbaa496e9cd0930841b883684442bf38bf48ab0
Benny Halevy <bhalevy@scylladb.com>
2020-02-06 09:09:43 +0200

    sstable: create_links: use seastar thread
    To simplify the implementation for error handling and
    replay support.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -2629,30 +2629,27 @@ std::vector<std::pair<component_type, sstring>> sstable::all_components() const 
 }
 
 future<> sstable::create_links(const sstring& dir, int64_t generation) const {
-    // TemporaryTOC is always first, TOC is always last
-    auto dst = sstable::filename(dir, _schema->ks_name(), _schema->cf_name(), _version, generation, _format, component_type::TemporaryTOC);
-    return sstable_write_io_check(::link_file, filename(component_type::TOC), dst).then([this, dir] {
-        return sstable_write_io_check(sync_directory, dir);
-    }).then([this, dir, generation] {
+    return seastar::async([this, dir = std::move(dir), generation] {
+        // TemporaryTOC is always first, TOC is always last
+        auto dst = sstable::filename(dir, _schema->ks_name(), _schema->cf_name(), _version, generation, _format, component_type::TemporaryTOC);
+        sstable_write_io_check(link_file, filename(component_type::TOC), dst).get();
+        sstable_write_io_check(sync_directory, dir).get();
         // FIXME: Should clean already-created links if we failed midway.
-        return parallel_for_each(all_components(), [this, dir, generation] (auto p) {
+        parallel_for_each(all_components(), [this, dir, generation] (auto p) {
             if (p.first == component_type::TOC) {
                 return make_ready_future<>();
             }
             auto src = sstable::filename(_dir, _schema->ks_name(), _schema->cf_name(), _version, _generation, _format, p.second);
             auto dst = sstable::filename(dir, _schema->ks_name(), _schema->cf_name(), _version, generation, _format, p.second);
-            return this->sstable_write_io_check(::link_file, std::move(src), std::move(dst));
-        });
-    }).then([this, dir] {
-        return sstable_write_io_check(sync_directory, dir);
-    }).then([dir, this, generation] {
+            return this->sstable_write_io_check(link_file, std::move(src), std::move(dst));
+        }).get();
+        sstable_write_io_check(sync_directory, dir).get();
         auto src = sstable::filename(dir, _schema->ks_name(), _schema->cf_name(), _version, generation, _format, component_type::TemporaryTOC);
-        auto dst = sstable::filename(dir, _schema->ks_name(), _schema->cf_name(), _version, generation, _format, component_type::TOC);
-        return sstable_write_io_check([&] {
+             dst = sstable::filename(dir, _schema->ks_name(), _schema->cf_name(), _version, generation, _format, component_type::TOC);
+        sstable_write_io_check([&] {
             return rename_file(src, dst);
-        });
-    }).then([this, dir] {
-        return sstable_write_io_check(sync_directory, dir);
+        }).get();
+        sstable_write_io_check(sync_directory, dir).get();
     });
 }
 
