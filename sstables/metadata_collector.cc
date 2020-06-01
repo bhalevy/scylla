4a192e56acf46c5fb785a657a2a4cdac28eb438a
Benny Halevy <bhalevy@scylladb.com>
2020-06-01 16:40:13 +0300

    sstables: keep whole clustering_key_prefix as min/max_column_names
    Currently we compare each min/max component independently.
    This may lead to suboptimal, inclusize clustering ranges
    that do not indicate any actual key we encounters.
    
    For example: ['a', 2], ['b', 1] will lead to min=['a', 1], max=['b', 2]
    instead of the keys themselves.
    
    This change keeps the min or max keys as a whole.
    
    Also, since we don't track partial key-prefix as possible with
    range tombstones, we do not allow unset suffixes starting at this commit.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- sstables/metadata_collector.cc
+++ sstables/metadata_collector.cc
@@ -24,29 +24,74 @@
 namespace sstables {
 
 void metadata_collector::do_update_min_max_components(const clustering_key_prefix& key) {
-    auto may_grow = [] (std::vector<bytes_opt>& v, size_t target_size) {
-        if (target_size > v.size()) {
-            v.resize(target_size);
-        }
-    };
-
     auto clustering_key_size = _schema.clustering_key_size();
+    auto& types = _schema.clustering_key_type()->types();
+    assert(types.size() == clustering_key_size);
+
     auto& min_seen = min_column_names();
     auto& max_seen = max_column_names();
-    may_grow(min_seen, clustering_key_size);
-    may_grow(max_seen, clustering_key_size);
 
-    auto& types = _schema.clustering_key_type()->types();
-    auto i = 0U;
+    // Set initial min==max values
+    if (!min_seen.size() || !max_seen.size()) {
+        min_seen.resize(clustering_key_size);
+        max_seen.resize(clustering_key_size);
+        size_t i = 0;
+        for (auto& value : key.components()) {
+            min_seen[i] = bytes(value.data(), value.size());
+            max_seen[i] = bytes(value.data(), value.size());
+            i++;
+        }
+        assert(i == clustering_key_size);
+        return;
+    }
+
+    size_t i = 0;
+    int cmp = 0;
+    size_t from = 0;
+
+    // compare key against min_seen
     for (auto& value : key.components()) {
         auto& type = types[i];
+        if (!cmp) {
+            cmp = type->compare(value, min_seen[i].value());
+            if (cmp == 0) {
+                i++;
+                continue;
+            } if (cmp > 0) {
+                from = i;
+                break;
+            }
+        }
+        min_seen[i] = bytes(value.data(), value.size());
+        i++;
+    }
 
-        if (!max_seen[i] || type->compare(value, max_seen[i].value()) > 0) {
-            max_seen[i] = bytes(value.data(), value.size());
+    // we're done if key <= min_seen
+    if (cmp <= 0) {
+        return;
+    }
+
+    i = 0;
+    cmp = 0;
+
+    // compare key against max_seen
+    for (auto& value : key.components()) {
+        // key == min_seen prefix up to `from`
+        // so it can't be > max_seen
+        if (i < from) {
+            continue;
         }
-        if (!min_seen[i] || type->compare(value, min_seen[i].value()) < 0) {
-            min_seen[i] = bytes(value.data(), value.size());
+        auto& type = types[i];
+        if (!cmp) {
+            cmp = type->compare(value, max_seen[i].value());
+            if (cmp == 0) {
+                i++;
+                continue;
+            } if (cmp < 0) {
+                break;
+            }
         }
+        max_seen[i] = bytes(value.data(), value.size());
         i++;
     }
 }
