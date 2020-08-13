3814cdadb4cba709fa0937b089c89c6e0b151122
Benny Halevy <bhalevy@scylladb.com>
2020-08-13 14:47:16 +0300

    abstract_replication_strategy: get_ranges: accept const token_metadata&
    Now that calculate_natural_endpoints can be passed a const token_metadata&
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- locator/abstract_replication_strategy.hh
+++ locator/abstract_replication_strategy.hh
@@ -116,11 +116,11 @@ public:
     dht::token_range_vector get_ranges_in_thread(inet_address ep) const;
 
     // Use the token_metadata provided by the caller instead of _token_metadata
-    dht::token_range_vector get_ranges(inet_address ep, token_metadata& tm) const;
+    dht::token_range_vector get_ranges(inet_address ep, const token_metadata& tm) const;
     // Caller must make sure that we can safely yield while accessing token_metadata
-    dht::token_range_vector get_ranges_in_thread(inet_address ep, token_metadata& tm) const;
+    dht::token_range_vector get_ranges_in_thread(inet_address ep, const token_metadata& tm) const;
 private:
-    dht::token_range_vector do_get_ranges(inet_address ep, token_metadata& tm, bool can_yield) const;
+    dht::token_range_vector do_get_ranges(inet_address ep, const token_metadata& tm, bool can_yield) const;
 
 public:
     // get_primary_ranges() returns the list of "primary ranges" for the given
