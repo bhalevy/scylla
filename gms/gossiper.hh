e238decda93ac3fdabe6d06957bf8fbd14b36558
Benny Halevy <bhalevy@scylladb.com>
2020-08-23 17:53:27 +0300

    main: start a shared_token_metadata
    And use it to get a token_metadata& compatible
    with current usage, until the services are converted to
    use token_metadata_ptr.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- gms/gossiper.hh
+++ gms/gossiper.hh
@@ -71,6 +71,7 @@ class config;
 
 namespace locator {
 class token_metadata;
+class shared_token_metadata;
 }
 
 namespace gms {
@@ -238,7 +239,7 @@ private:
     // The value must be kept alive until completes and not change.
     future<> replicate(inet_address, application_state key, const versioned_value& value);
 public:
-    explicit gossiper(abort_source& as, feature_service& features, const locator::token_metadata& tokens, netw::messaging_service& ms, db::config& cfg);
+    explicit gossiper(abort_source& as, feature_service& features, const locator::shared_token_metadata& tokens, netw::messaging_service& ms, db::config& cfg);
 
     void set_last_processed_message_at();
     void set_last_processed_message_at(clk::time_point tp);
