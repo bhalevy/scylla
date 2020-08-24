30c58036c5ea877861fcc92498f9a58f92b3976f
Benny Halevy <bhalevy@scylladb.com>
2020-08-24 12:01:30 +0300

    abstract_replication_strategy: convert to shared_token_metadata
    To facilitate that, keep a const shared_token_metadata& in class database
    rather than a const token_metadata&
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- gms/gossiper.hh
+++ gms/gossiper.hh
@@ -237,7 +237,7 @@ private:
     // The value must be kept alive until completes and not change.
     future<> replicate(inet_address, application_state key, const versioned_value& value);
 public:
-    explicit gossiper(abort_source& as, feature_service& features, const locator::shared_token_metadata& tokens, netw::messaging_service& ms, db::config& cfg);
+    explicit gossiper(abort_source& as, feature_service& features, const locator::shared_token_metadata& stm, netw::messaging_service& ms, db::config& cfg);
 
     void check_seen_seeds();
 
