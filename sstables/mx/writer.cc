23f89d52d46dbcccc26d5dc7bc9908174d48e532
Benny Halevy <bhalevy@scylladb.com>
2020-11-22 13:03:50 +0200

    sstables: store large_data_stats in scylla_metadata
    Store the large data statistics in the scylla_metadata component.
    
    These will be retrieved when loading the sstable and be
    used for determining whether to delete the corresponding
    large data entries upon sstable deletion.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- sstables/mx/writer.cc
+++ sstables/mx/writer.cc
@@ -1517,7 +1517,7 @@ void writer::consume_end_of_stream() {
         features.disable(sstable_feature::CorrectStaticCompact);
     }
     run_identifier identifier{_run_identifier};
-    _sst.write_scylla_metadata(_pc, _shard, std::move(features), std::move(identifier));
+    _sst.write_scylla_metadata(_pc, _shard, std::move(features), std::move(identifier), std::move(_large_data_stats));
     if (!_cfg.leave_unsealed) {
         _sst.seal_sstable(_cfg.backup).get();
     }
