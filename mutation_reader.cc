1b518f1815a0856f23e2f052486ad854a0237a62
Benny Halevy <bhalevy@scylladb.com>
2020-12-30 16:03:06 +0200

    mutation_reader: multishard_combining_reader: implement abort and close
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- mutation_reader.cc
+++ mutation_reader.cc
@@ -1922,6 +1922,8 @@ public:
     virtual future<> next_partition() override;
     virtual future<> fast_forward_to(const dht::partition_range& pr, db::timeout_clock::time_point timeout) override;
     virtual future<> fast_forward_to(position_range pr, db::timeout_clock::time_point timeout) override;
+    virtual future<> abort(std::exception_ptr ex) noexcept override;
+    virtual future<> close() noexcept override;
 };
 
 void multishard_combining_reader::on_partition_range_change(const dht::partition_range& pr) {
@@ -2058,6 +2060,18 @@ future<> multishard_combining_reader::fast_forward_to(const dht::partition_range
 
 future<> multishard_combining_reader::fast_forward_to(position_range pr, db::timeout_clock::time_point timeout) {
     return make_exception_future<>(make_backtraced_exception_ptr<std::bad_function_call>());
+}
+
+future<> multishard_combining_reader::abort(std::exception_ptr ex) noexcept {
+    return parallel_for_each(_shard_readers, [ex = std::move(ex)] (lw_shared_ptr<shard_reader>& sr) {
+        return sr->abort(ex);
+    });
+}
+
+future<> multishard_combining_reader::close() noexcept {
+    return parallel_for_each(_shard_readers, [] (lw_shared_ptr<shard_reader>& sr) {
+        return sr->close();
+    });
 }
 
 reader_concurrency_semaphore::inactive_read_handle
