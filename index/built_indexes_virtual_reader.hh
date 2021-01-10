c79cf8a41a24c6ee3bc978cebce635e84b4d6aa1
Benny Halevy <bhalevy@scylladb.com>
2021-01-10 12:22:33 +0200

    flat_mutation_reader: add impl::description
    Pass a description sstring when constructing flat_mutation_reader::impl.
    Will be used in debug messages and on_internal_error checks.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- index/built_indexes_virtual_reader.hh
+++ index/built_indexes_virtual_reader.hh
@@ -54,7 +54,7 @@ class built_indexes_virtual_reader {
                 tracing::trace_state_ptr trace_state,
                 streamed_mutation::forwarding fwd,
                 mutation_reader::forwarding fwd_mr)
-                : flat_mutation_reader::impl(std::move(schema), permit)
+                : flat_mutation_reader::impl(std::move(schema), permit, "built_indexes_reader")
                 , _db(db)
                 , _underlying(built_views.make_reader(
                         built_views.schema(),
