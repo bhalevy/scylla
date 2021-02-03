67b9bbe8bba0f6fe170a9f1de5a601a37e5a30f9
Benny Halevy <bhalevy@scylladb.com>
2021-02-03 10:26:32 +0200

    flat_mutation_reader: make sure to close reader passed to read_mutation_from_flat_mutation_reader
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- table.cc
+++ table.cc
@@ -117,7 +117,7 @@ table::make_sstable_reader(schema_ptr s,
 future<table::const_mutation_partition_ptr>
 table::find_partition(schema_ptr s, reader_permit permit, const dht::decorated_key& key) const {
     return do_with(dht::partition_range::make_singular(key), [s = std::move(s), permit = std::move(permit), this] (auto& range) mutable {
-        return do_with(this->make_reader(std::move(s), std::move(permit), range), [] (flat_mutation_reader& reader) {
+        return with_flat_mutation_reader(this->make_reader(std::move(s), std::move(permit), range), [] (flat_mutation_reader& reader) {
             return read_mutation_from_flat_mutation_reader(reader, db::no_timeout).then([] (mutation_opt&& mo) -> std::unique_ptr<const mutation_partition> {
                 if (!mo) {
                     return {};
