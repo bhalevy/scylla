656d34514d543dd63db255d66411de2c4b5294c3
Benny Halevy <bhalevy@scylladb.com>
2021-02-14 20:23:34 +0200

    reader_concurrency_semaphore: unregister_inactive_read: close reader also on internal error
    If unregister_inactive_read we call on_internal_error
    that may end up throwing rather than aborting.
    
    In this case, "forward" the reader close to the right semaphore.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- reader_concurrency_semaphore.cc
+++ reader_concurrency_semaphore.cc
@@ -431,6 +431,7 @@ flat_mutation_reader_opt reader_concurrency_semaphore::unregister_inactive_read(
         return {};
     }
     if (irh._sem != this) {
+      try {
         on_internal_error(rcslog, fmt::format(
                     "reader_concurrency_semaphore::unregister_inactive_read(): "
                     "attempted to unregister an inactive read with a handle belonging to another semaphore: "
@@ -439,6 +440,12 @@ flat_mutation_reader_opt reader_concurrency_semaphore::unregister_inactive_read(
                     reinterpret_cast<uintptr_t>(this),
                     irh._sem->name(),
                     reinterpret_cast<uintptr_t>(irh._sem)));
+      } catch (...) {
+        auto irp = std::move(irh._irp);
+        irp->unlink();
+        irh._sem->close_reader(std::move(irp->reader));
+        throw;
+      }
     }
 
     assert(!_stopped);
