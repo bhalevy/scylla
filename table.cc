41454035d1ecb4f527331303a197d10c01fabe86
Benny Halevy <bhalevy@scylladb.com>
2021-03-11 11:28:42 +0200

    table: coroutinize table::query

--- table.cc
+++ table.cc
@@ -1766,28 +1766,43 @@ table::query(schema_ptr s,
     utils::latency_counter lc;
     _stats.reads.set_latency(lc);
     const auto short_read_allwoed = query::short_read(cmd.slice.options.contains<query::partition_slice::option::allow_short_read>());
-    auto f = opts.request == query::result_request::only_digest
-             ? memory_limiter.new_digest_read(*cmd.max_result_size, short_read_allwoed) : memory_limiter.new_data_read(*cmd.max_result_size, short_read_allwoed);
-    return f.then([this, lc, s = std::move(s), &cmd, class_config, opts, &partition_ranges,
-            trace_state = std::move(trace_state), timeout, cache_ctx = std::move(cache_ctx),
-            leave = std::move(leave)] (query::result_memory_accounter accounter) mutable {
+    auto accounter = co_await (opts.request == query::result_request::only_digest
+             ? memory_limiter.new_digest_read(*cmd.max_result_size, short_read_allwoed) : memory_limiter.new_data_read(*cmd.max_result_size, short_read_allwoed));
         auto qs_ptr = std::make_unique<query_state>(std::move(s), cmd, opts, partition_ranges, std::move(accounter));
         auto& qs = *qs_ptr;
-        return do_until(std::bind(&query_state::done, &qs), [this, &qs, class_config, trace_state = std::move(trace_state), timeout, cache_ctx = std::move(cache_ctx)] {
+        while (!qs.done()) {
             auto&& range = *qs.current_partition_range++;
-            return data_query(qs.schema, as_mutation_source(), range, qs.cmd.slice, qs.remaining_rows(),
+            auto ms = as_mutation_source();
+            co_await data_query(qs.schema, ms, range, qs.cmd.slice, qs.remaining_rows(),
                               qs.remaining_partitions(), qs.cmd.timestamp, qs.builder, timeout, class_config, trace_state, cache_ctx);
-        }).then([qs_ptr = std::move(qs_ptr), &qs] {
-            return make_ready_future<lw_shared_ptr<query::result>>(
-                    make_lw_shared<query::result>(qs.builder.build()));
-        }).finally([lc, this, leave = std::move(leave)]() mutable {
+        }
+        std::exception_ptr ex;
+        lw_shared_ptr<query::result> res;
+        try {
+            res = make_lw_shared<query::result>(qs.builder.build());
+        } catch (...) {
+            ex = std::current_exception();
+        }
+        qs_ptr.reset();
+        auto finally = [lc, this, leave = std::move(leave)]() mutable {
             _stats.reads.mark(lc);
             if (lc.is_start()) {
                 _stats.estimated_read.add(lc.latency());
             }
             // "leave" is destroyed here
-        });
-    });
+        };
+    try {
+        finally();
+    } catch (...) {
+        if (ex) {
+            throw seastar::nested_exception(std::current_exception(), std::move(ex));
+        }
+        throw;
+    }
+    if (ex) {
+        std::rethrow_exception(std::move(ex));
+    }
+    co_return res;
 }
 
 mutation_source
