scylladb/scylladb

commit c3b8757fa1c5aa28b4fec9aef61a8820740dd97b

sstable_set: move all() implementation into sstable_set_impl
The main motivation behind this is that by moving all() impl into
sstable_set_impl, sstable_set no longer needs to maintain a list
with all sstables, which in turn may disagree with the respective
sstable_set_impl.

This will be very important for compound_sstable_set_impl which
will be built from existing sets, and will implement all() by
combining the all() of its managed sets.
Without this patch, we'd have to insert the same sstable at
both compound set and also the set managed by it, to guarantee
all() of compound set would return the correct data, which would
be expensive and error prone.

Signed-off-by: Raphael S. Carvalho <raphaelsc@scylladb.com>

sstables/sstable_set.cc
@@ -63,16 +63,14 @@ std::ostream& operator<<(std::ostream& os, const sstables::sstable_run& run) {
     return os;
 }
 
-sstable_set::sstable_set(std::unique_ptr<sstable_set_impl> impl, schema_ptr s, lw_shared_ptr<sstable_list> all)
+sstable_set::sstable_set(std::unique_ptr<sstable_set_impl> impl, schema_ptr s)
         : _impl(std::move(impl))
-        , _schema(std::move(s))
-        , _all(std::move(all)) {
+        , _schema(std::move(s)) {
 }
 
 sstable_set::sstable_set(const sstable_set& x)
         : _impl(x._impl->clone())
         , _schema(x._schema)
-        , _all(make_lw_shared<sstable_list>(*x._all))
         , _all_runs(x._all_runs) {
 }
 
@@ -103,17 +101,16 @@ sstable_set::select_sstable_runs(const std::vector<shared_sstable>& sstables) co
     }));
 }
 
+lw_shared_ptr<sstable_list>
+sstable_set::all() const {
+    return _impl->all();
+}
+
 void
 sstable_set::insert(shared_sstable sst) {
     _impl->insert(sst);
     try {
-        _all->insert(sst);
-        try {
-            _all_runs[sst->run_identifier()].insert(sst);
-        } catch (...) {
-            _all->erase(sst);
-            throw;
-        }
+        _all_runs[sst->run_identifier()].insert(sst);
     } catch (...) {
         _impl->erase(sst);
         throw;
@@ -123,7 +120,6 @@ sstable_set::insert(shared_sstable sst) {
 void
 sstable_set::erase(shared_sstable sst) {
     _impl->erase(sst);
-    _all->erase(sst);
     _all_runs[sst->run_identifier()].erase(sst);
 }
 
@@ -225,8 +221,9 @@ dht::partition_range partitioned_sstable_set::to_partition_range(const dht::ring
     return dht::partition_range::make(std::move(lower_bound), std::move(upper_bound));
 }
 
-partitioned_sstable_set::partitioned_sstable_set(schema_ptr schema, bool use_level_metadata)
+partitioned_sstable_set::partitioned_sstable_set(schema_ptr schema, lw_shared_ptr<sstable_list> all, bool use_level_metadata)
         : _schema(std::move(schema))
+        , _all(std::move(all))
         , _use_level_metadata(use_level_metadata) {
 }
 
@@ -247,16 +244,26 @@ std::vector<shared_sstable> partitioned_sstable_set::select(const dht::partition
     return r;
 }
 
+lw_shared_ptr<sstable_list> partitioned_sstable_set::all() const {
+    return _all;
+}
+
 void partitioned_sstable_set::insert(shared_sstable sst) {
-    if (store_as_unleveled(sst)) {
-        _unleveled_sstables.push_back(std::move(sst));
-    } else {
-        _leveled_sstables_change_cnt++;
-        _leveled_sstables.add({make_interval(*sst), value_set({sst})});
+    _all->insert(sst);
+    try {
+        if (store_as_unleveled(sst)) {
+            _unleveled_sstables.push_back(std::move(sst));
+        } else {
+            _leveled_sstables_change_cnt++;
+            _leveled_sstables.add({make_interval(*sst), value_set({sst})});
+        }
+    } catch (...) {
+        _all->erase(sst);
     }
 }
 
 void partitioned_sstable_set::erase(shared_sstable sst) {
+    _all->erase(sst);
     if (store_as_unleveled(sst)) {
         _unleveled_sstables.erase(std::remove(_unleveled_sstables.begin(), _unleveled_sstables.end(), sst), _unleveled_sstables.end());
     } else {
@@ -344,6 +351,10 @@ std::unique_ptr<sstable_set_impl> time_series_sstable_set::clone() const {
 
 std::vector<shared_sstable> time_series_sstable_set::select(const dht::partition_range& range) const {
     return boost::copy_range<std::vector<shared_sstable>>(*_sstables | boost::adaptors::map_values);
+}
+
+lw_shared_ptr<sstable_list> time_series_sstable_set::all() const {
+    return make_lw_shared<sstable_list>(boost::copy_range<sstable_list>(*_sstables | boost::adaptors::map_values));
 }
 
 // O(log n)
@@ -491,11 +502,11 @@ std::unique_ptr<incremental_selector_impl> partitioned_sstable_set::make_increme
 
 std::unique_ptr<sstable_set_impl> compaction_strategy_impl::make_sstable_set(schema_ptr schema) const {
     // with use_level_metadata enabled, L0 sstables will not go to interval map, which suits well STCS.
-    return std::make_unique<partitioned_sstable_set>(schema, true);
+    return std::make_unique<partitioned_sstable_set>(schema, make_lw_shared<sstable_list>(), true);
 }
 
 std::unique_ptr<sstable_set_impl> leveled_compaction_strategy::make_sstable_set(schema_ptr schema) const {
-    return std::make_unique<partitioned_sstable_set>(std::move(schema));
+    return std::make_unique<partitioned_sstable_set>(std::move(schema), make_lw_shared<sstable_list>());
 }
 
 std::unique_ptr<sstable_set_impl> time_window_compaction_strategy::make_sstable_set(schema_ptr schema) const {
@@ -503,15 +514,14 @@ std::unique_ptr<sstable_set_impl> time_window_compaction_strategy::make_sstable_
 }
 
 sstable_set make_partitioned_sstable_set(schema_ptr schema, lw_shared_ptr<sstable_list> all, bool use_level_metadata) {
-    return sstable_set(std::make_unique<partitioned_sstable_set>(schema, use_level_metadata), schema, std::move(all));
+    return sstable_set(std::make_unique<partitioned_sstable_set>(schema, std::move(all), use_level_metadata), schema);
 }
 
 sstable_set
 compaction_strategy::make_sstable_set(schema_ptr schema) const {
     return sstable_set(
             _compaction_strategy_impl->make_sstable_set(schema),
-            schema,
-            make_lw_shared<sstable_list>());
+            schema);
 }
 
 using sstable_reader_factory_type = std::function<flat_mutation_reader(shared_sstable&, const dht::partition_range& pr)>;

sstables/sstable_set_impl.hh
@@ -39,6 +39,7 @@ public:
     virtual ~sstable_set_impl() {}
     virtual std::unique_ptr<sstable_set_impl> clone() const = 0;
     virtual std::vector<shared_sstable> select(const dht::partition_range& range) const = 0;
+    virtual lw_shared_ptr<sstable_list> all() const = 0;
     virtual void insert(shared_sstable sst) = 0;
     virtual void erase(shared_sstable sst) = 0;
     virtual std::unique_ptr<incremental_selector_impl> make_incremental_selector() const = 0;
@@ -67,6 +68,7 @@ private:
     schema_ptr _schema;
     std::vector<shared_sstable> _unleveled_sstables;
     interval_map_type _leveled_sstables;
+    lw_shared_ptr<sstable_list> _all;
     // Change counter on interval map for leveled sstables which is used by
     // incremental selector to determine whether or not to invalidate iterators.
     uint64_t _leveled_sstables_change_cnt = 0;
@@ -84,10 +86,11 @@ public:
     static dht::ring_position to_ring_position(const compatible_ring_position_or_view& crp);
     static dht::partition_range to_partition_range(const interval_type& i);
     static dht::partition_range to_partition_range(const dht::ring_position_view& pos, const interval_type& i);
-    explicit partitioned_sstable_set(schema_ptr schema, bool use_level_metadata = true);
+    explicit partitioned_sstable_set(schema_ptr schema, lw_shared_ptr<sstable_list> all, bool use_level_metadata = true);
 
     virtual std::unique_ptr<sstable_set_impl> clone() const override;
     virtual std::vector<shared_sstable> select(const dht::partition_range& range) const override;
+    virtual lw_shared_ptr<sstable_list> all() const override;
     virtual void insert(shared_sstable sst) override;
     virtual void erase(shared_sstable sst) override;
     virtual std::unique_ptr<incremental_selector_impl> make_incremental_selector() const override;
@@ -109,6 +112,7 @@ public:
 
     virtual std::unique_ptr<sstable_set_impl> clone() const override;
     virtual std::vector<shared_sstable> select(const dht::partition_range& range = query::full_partition_range) const override;
+    virtual lw_shared_ptr<sstable_list> all() const override;
     virtual void insert(shared_sstable sst) override;
     virtual void erase(shared_sstable sst) override;
     virtual std::unique_ptr<incremental_selector_impl> make_incremental_selector() const override;

sstables/sstable_set.hh
@@ -52,13 +52,10 @@ public:
 class sstable_set : public enable_lw_shared_from_this<sstable_set> {
     std::unique_ptr<sstable_set_impl> _impl;
     schema_ptr _schema;
-    // used to support column_family::get_sstable(), which wants to return an sstable_list
-    // that has a reference somewhere
-    lw_shared_ptr<sstable_list> _all;
     std::unordered_map<utils::UUID, sstable_run> _all_runs;
 public:
     ~sstable_set();
-    sstable_set(std::unique_ptr<sstable_set_impl> impl, schema_ptr s, lw_shared_ptr<sstable_list> all);
+    sstable_set(std::unique_ptr<sstable_set_impl> impl, schema_ptr s);
     sstable_set(const sstable_set&);
     sstable_set(sstable_set&&) noexcept;
     sstable_set& operator=(const sstable_set&);
@@ -66,7 +63,8 @@ public:
     std::vector<shared_sstable> select(const dht::partition_range& range) const;
     // Return all runs which contain any of the input sstables.
     std::vector<sstable_run> select_sstable_runs(const std::vector<shared_sstable>& sstables) const;
-    lw_shared_ptr<sstable_list> all() const { return _all; }
+    // Return all sstables. It's not guaranteed that sstable_set will keep a reference to the returned list, so user should keep it.
+    lw_shared_ptr<sstable_list> all() const;
     void insert(shared_sstable sst);
     void erase(shared_sstable sst);
 

bhalevy/scylla

commit ebc2b827b3ce71bacaf04997d8d88a6e2bb1ca8b

sstable_set: partitioned_sstable_set: clone: do clone all sstables
The existing implementation wrongfully shares _all sstables
rather than cloning it. This caused a use-after-free
in `repair_meta::do_estimate_partitions_on_local_shard`
when traversing a shared sstable_set, during which
`table::make_reader_excluding_sstables` erased an entry.
The erase should have happened on a cloned copy
of the sstable_list, not on a shared copy.

The regression was introduced in
c3b8757fa1c5aa28b4fec9aef61a8820740dd97b.

Added a unit test that reproduces the share-on-copy issue
for partitioned_stable_set (sstables::sstable_set).

Fixes #8274

Test: unit(release, debug)
DTest: materialized_views_test.py:TestMaterializedViews.simple_repair_test(debug)
Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

configure.py
@@ -415,6 +415,7 @@ scylla_tests = set([
     'test/boost/double_decker_test',
     'test/boost/stall_free_test',
     'test/boost/raft_sys_table_storage_test',
+    'test/boost/sstable_set_test',
     'test/manual/ec2_snitch_test',
     'test/manual/enormous_table_scan_test',
     'test/manual/gce_snitch_test',

sstables/sstable_set.cc
@@ -229,8 +229,18 @@ partitioned_sstable_set::partitioned_sstable_set(schema_ptr schema, lw_shared_pt
         , _use_level_metadata(use_level_metadata) {
 }
 
+partitioned_sstable_set::partitioned_sstable_set(schema_ptr schema, const std::vector<shared_sstable>& unleveled_sstables, const interval_map_type& leveled_sstables,
+        const lw_shared_ptr<sstable_list>& all, const std::unordered_map<utils::UUID, sstable_run>& all_runs, bool use_level_metadata)
+        : _schema(schema)
+        , _unleveled_sstables(unleveled_sstables)
+        , _leveled_sstables(leveled_sstables)
+        , _all(make_lw_shared<sstable_list>(*all))
+        , _all_runs(all_runs)
+        , _use_level_metadata(use_level_metadata) {
+}
+
 std::unique_ptr<sstable_set_impl> partitioned_sstable_set::clone() const {
-    return std::make_unique<partitioned_sstable_set>(*this);
+    return std::make_unique<partitioned_sstable_set>(_schema, _unleveled_sstables, _leveled_sstables, _all, _all_runs, _use_level_metadata);
 }
 
 std::vector<shared_sstable> partitioned_sstable_set::select(const dht::partition_range& range) const {

sstables/sstable_set_impl.hh
@@ -90,7 +90,17 @@ public:
     static dht::ring_position to_ring_position(const compatible_ring_position_or_view& crp);
     static dht::partition_range to_partition_range(const interval_type& i);
     static dht::partition_range to_partition_range(const dht::ring_position_view& pos, const interval_type& i);
+
+    partitioned_sstable_set(const partitioned_sstable_set&) = delete;
     explicit partitioned_sstable_set(schema_ptr schema, lw_shared_ptr<sstable_list> all, bool use_level_metadata = true);
+    // For cloning the partitioned_sstable_set (makes a deep copy, including *_all)
+    explicit partitioned_sstable_set(
+        schema_ptr schema,
+        const std::vector<shared_sstable>& unleveled_sstables,
+        const interval_map_type& leveled_sstables,
+        const lw_shared_ptr<sstable_list>& all,
+        const std::unordered_map<utils::UUID, sstable_run>& all_runs,
+        bool use_level_metadata);
 
     virtual std::unique_ptr<sstable_set_impl> clone() const override;
     virtual std::vector<shared_sstable> select(const dht::partition_range& range) const override;

test/boost/sstable_set_test.cc
@@ -1,0 +1,101 @@
+/*
+ * Copyright (C) 2021 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+#include <seastar/testing/test_case.hh>
+
+#include "sstables/sstable_set_impl.hh"
+#include "sstables/sstable_set.hh"
+#include "sstables/sstables.hh"
+#include "test/lib/simple_schema.hh"
+
+static sstables::sstable_set make_sstable_set(schema_ptr schema, lw_shared_ptr<sstable_list> all = {}, bool use_level_metadata = true) {
+    return sstables::sstable_set(std::make_unique<partitioned_sstable_set>(schema, std::move(all), use_level_metadata), schema);
+}
+
+SEASTAR_TEST_CASE(test_sstables_sstable_set_read_modify_write) {
+    return test_setup::do_with_tmp_directory([] (test_env& env, sstring tmpdir_path) {
+        simple_schema ss;
+        auto s = ss.schema();
+
+        auto pk = ss.make_pkey(make_local_key(s));
+        auto mut = mutation(s, pk);
+        ss.add_row(mut, ss.make_ckey(0), "val");
+        int gen = 1;
+
+        auto mr = flat_mutation_reader_from_mutations(tests::make_permit(), {mut});
+        auto sst1 = env.make_sstable(s, tmpdir_path, gen++);
+        sstable_writer_config cfg = env.manager().configure_writer("");
+        sst1->write_components(std::move(mr), 0, s, std::move(cfg), encoding_stats{}).get();
+        sst1->load().get();
+
+        auto ss1 = make_lw_shared<sstables::sstable_set>(make_sstable_set(ss.schema(), make_lw_shared<sstable_list>({sst1})));
+        BOOST_REQUIRE_EQUAL(ss1->all()->size(), 1);
+
+        // Test that a random sstable_origin is stored and retrieved properly.
+        mr = flat_mutation_reader_from_mutations(tests::make_permit(), {mut});
+        auto sst2 = env.make_sstable(s, tmpdir_path, gen++);
+        sst2->write_components(std::move(mr), 0, s, std::move(cfg), encoding_stats{}).get();
+        sst2->load().get();
+
+        auto ss2 = make_lw_shared<sstables::sstable_set>(*ss1);
+        ss2->insert(sst2);
+        BOOST_REQUIRE_EQUAL(ss2->all()->size(), 2);
+        BOOST_REQUIRE_EQUAL(ss1->all()->size(), 1);
+
+        return make_ready_future<>();
+    });
+}
+
+SEASTAR_TEST_CASE(test_time_series_sstable_set_read_modify_write) {
+    return test_setup::do_with_tmp_directory([] (test_env& env, sstring tmpdir_path) {
+        simple_schema ss;
+        auto s = ss.schema();
+
+        auto pk = ss.make_pkey(make_local_key(s));
+        auto mut = mutation(s, pk);
+        ss.add_row(mut, ss.make_ckey(0), "val");
+        int gen = 1;
+
+        auto mr = flat_mutation_reader_from_mutations(tests::make_permit(), {mut});
+        auto sst1 = env.make_sstable(s, tmpdir_path, gen++);
+        sstable_writer_config cfg = env.manager().configure_writer("");
+        sst1->write_components(std::move(mr), 0, s, std::move(cfg), encoding_stats{}).get();
+        sst1->load().get();
+
+        auto ss1 = make_lw_shared<time_series_sstable_set>(ss.schema());
+        ss1->insert(sst1);
+        BOOST_REQUIRE_EQUAL(ss1->all()->size(), 1);
+
+        // Test that a random sstable_origin is stored and retrieved properly.
+        mr = flat_mutation_reader_from_mutations(tests::make_permit(), {mut});
+        auto sst2 = env.make_sstable(s, tmpdir_path, gen++);
+        sst2->write_components(std::move(mr), 0, s, std::move(cfg), encoding_stats{}).get();
+        sst2->load().get();
+
+        auto ss2 = make_lw_shared<time_series_sstable_set>(*ss1);
+        ss2->insert(sst2);
+        BOOST_REQUIRE_EQUAL(ss2->all()->size(), 2);
+        BOOST_REQUIRE_EQUAL(ss1->all()->size(), 1);
+
+        return make_ready_future<>();
+    });
+}