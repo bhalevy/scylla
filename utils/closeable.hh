5dd5dc66fa2dab052907e586aec1698a83b8b520
Benny Halevy <bhalevy@scylladb.com>
2021-03-18 08:24:37 +0200

    utils: add deferred_close
    Close the object when the respected
    deferred_close object is destroyed.
    
    In addition, provide a close_now method
    can be used to perform the cleanup once,
    while the deferred_close object is still alive.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

---
+++ utils/closeable.hh
@@ -0,0 +1,74 @@
+/*
+ * Copyright (C) 2020 ScyllaDB
+ */
+
+/*
+ * This file is part of Scylla.
+ *
+ * Scylla is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Scylla is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <seastar/core/future.hh>
+
+namespace seastar {
+
+template <typename Object>
+concept closeable = requires (Object o) {
+    { o.close() } -> std::same_as<future<>>;
+};
+
+/// Template helper to close \c obj when destroyed.
+///
+/// \tparam Object a class exposing a \c close() method that returns a \c future<>
+///         that is called when the controller is destroyed.
+template <typename Object>
+requires closeable<Object>
+class close_controller {
+    Object& _obj;
+    bool _closed = false;
+
+    void do_close() noexcept {
+        if (!_closed) {
+            _closed = true;
+            _obj.close().get();
+        }
+    }
+public:
+    close_controller(Object& obj) noexcept : _obj(obj) {}
+    ~close_controller() {
+        do_close();
+    }
+    /// Close \c obj once now.
+    void close_now() noexcept {
+        assert(!_closed);
+        do_close();
+    }
+};
+
+/// Auto-close an object.
+///
+/// \param obj object to auto-close.
+///
+/// \return A deferred action that closes \c obj.
+///
+/// \note Can be used only in a seatstar thread.
+template <typename Object>
+requires closeable<Object>
+inline auto deferred_close(Object& obj) {
+    return close_controller(obj);
+}
+
+} // namespace seastar
