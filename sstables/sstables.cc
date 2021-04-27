34360dab50489ed4385d7deef254cfe44038f39f
Benny Halevy <bhalevy@scylladb.com>
2021-04-27 12:17:15 +0300

    sstables: create_sharding_metadata: assert partition range is fully bounded
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- sstables/sstables.cc
+++ sstables/sstables.cc
@@ -1513,6 +1513,8 @@ static
 sharding_metadata
 create_sharding_metadata(schema_ptr schema, const dht::decorated_key& first_key, const dht::decorated_key& last_key, shard_id shard) {
     auto prange = dht::partition_range::make(dht::ring_position(first_key), dht::ring_position(last_key));
+    assert(prange.start());
+    assert(prange.end());
     auto sm = sharding_metadata();
     auto&& ranges = dht::split_range_to_single_shard(*schema, prange, shard).get0();
     sm.token_ranges.elements.reserve(ranges.size());
