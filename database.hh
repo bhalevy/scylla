c8f950910617acaad7b7a388fdd9219f3e83d3fe
Benny Halevy <bhalevy@scylladb.com>
2021-05-09 18:18:05 +0300

    memtable_list: get rid of seal_active_memtable_delayed
    This path is currently unused.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- database.hh
+++ database.hh
@@ -160,7 +160,6 @@ public:
 private:
     std::vector<shared_memtable> _memtables;
     seal_immediate_fn_type _seal_immediate_fn;
-    seal_delayed_fn_type _seal_delayed_fn;
     await_pending_flushes_fn_type _await_pending_flushes_fn;
     std::function<schema_ptr()> _current_schema;
     dirty_memory_manager* _dirty_memory_manager;
@@ -170,7 +169,6 @@ private:
 public:
     memtable_list(
             seal_immediate_fn_type seal_immediate_fn,
-            seal_delayed_fn_type seal_delayed_fn,
             await_pending_flushes_fn_type await_pending_flushes_fn,
             std::function<schema_ptr()> cs,
             dirty_memory_manager* dirty_memory_manager,
@@ -178,7 +176,6 @@ public:
             seastar::scheduling_group compaction_scheduling_group = seastar::current_scheduling_group())
         : _memtables({})
         , _seal_immediate_fn(seal_immediate_fn)
-        , _seal_delayed_fn(seal_delayed_fn)
         , _await_pending_flushes_fn(await_pending_flushes_fn)
         , _current_schema(cs)
         , _dirty_memory_manager(dirty_memory_manager)
@@ -187,20 +184,10 @@ public:
         add_memtable();
     }
 
-    memtable_list(
-            seal_immediate_fn_type seal_immediate_fn,
-            await_pending_flushes_fn_type await_pending_flushes_fn,
-            std::function<schema_ptr()> cs,
-            dirty_memory_manager* dirty_memory_manager,
-            table_stats& table_stats,
-            seastar::scheduling_group compaction_scheduling_group = seastar::current_scheduling_group())
-        : memtable_list(std::move(seal_immediate_fn), {}, std::move(await_pending_flushes_fn), std::move(cs), dirty_memory_manager, table_stats, compaction_scheduling_group) {
-    }
-
     memtable_list(std::function<schema_ptr()> cs, dirty_memory_manager* dirty_memory_manager,
             table_stats& table_stats,
             seastar::scheduling_group compaction_scheduling_group = seastar::current_scheduling_group())
-        : memtable_list({}, {}, [] { return make_ready_future<>(); }, std::move(cs), dirty_memory_manager, table_stats, compaction_scheduling_group) {
+        : memtable_list({}, [] { return make_ready_future<>(); }, std::move(cs), dirty_memory_manager, table_stats, compaction_scheduling_group) {
     }
 
     bool may_flush() const {
@@ -237,13 +224,6 @@ public:
 
     future<> seal_active_memtable_immediate(flush_permit&& permit) {
         return _seal_immediate_fn(std::move(permit));
-    }
-
-    future<> seal_active_memtable_delayed() {
-        if (_seal_delayed_fn) {
-            return _seal_delayed_fn();
-        }
-        return request_flush();
     }
 
     future<> await_pending_flushes() {
