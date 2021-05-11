58e28b445f0572477369c1c93b127df22d338fc2
Benny Halevy <bhalevy@scylladb.com>
2021-05-11 08:53:48 +0300

    cql_serialization_format.hh: mark functions noexcept
    They trivially are so.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- cql_serialization_format.hh
+++ cql_serialization_format.hh
@@ -37,17 +37,17 @@ class cql_serialization_format {
     cql_protocol_version_type _version;
 public:
     static constexpr cql_protocol_version_type latest_version = 4;
-    explicit cql_serialization_format(cql_protocol_version_type version) : _version(version) {}
-    static cql_serialization_format latest() { return cql_serialization_format{latest_version}; }
-    static cql_serialization_format internal() { return latest(); }
-    bool using_32_bits_for_collections() const { return _version >= 3; }
-    bool operator==(cql_serialization_format x) const { return _version == x._version; }
-    bool operator!=(cql_serialization_format x) const { return !operator==(x); }
-    cql_protocol_version_type protocol_version() const { return _version; }
+    explicit cql_serialization_format(cql_protocol_version_type version) noexcept : _version(version) {}
+    static cql_serialization_format latest() noexcept { return cql_serialization_format{latest_version}; }
+    static cql_serialization_format internal() noexcept { return latest(); }
+    bool using_32_bits_for_collections() const noexcept { return _version >= 3; }
+    bool operator==(cql_serialization_format x) const noexcept { return _version == x._version; }
+    bool operator!=(cql_serialization_format x) const noexcept { return !operator==(x); }
+    cql_protocol_version_type protocol_version() const noexcept { return _version; }
     friend std::ostream& operator<<(std::ostream& out, const cql_serialization_format& sf) {
         return out << static_cast<int>(sf._version);
     }
-    bool collection_format_unchanged(cql_serialization_format other = cql_serialization_format::latest()) const {
+    bool collection_format_unchanged(cql_serialization_format other = cql_serialization_format::latest()) const noexcept {
         return using_32_bits_for_collections() == other.using_32_bits_for_collections();
     }
 };
