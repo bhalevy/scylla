0c85d9ae9e923980d7bbbc55d75ae0bf64183e04
Benny Halevy <bhalevy@scylladb.com>
2021-09-22 17:32:27 +0300

    abstract_replication_strategy: get rid of do_calculate_natural_endpoints
    It is no longer in use.
    
    And with it, the virtual calculate_natural_endpoint_sync method
    of which it was the only caller.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- locator/abstract_replication_strategy.hh
+++ locator/abstract_replication_strategy.hh
@@ -184,12 +184,6 @@ public:
 
     // Apply the replication strategy over the current configuration and the given token_metadata.
     future<effective_replication_map_ptr> make_effective_replication_map(token_metadata_ptr tmptr) const;
-private:
-    // FIXME: temporary, until all users are converted to use the async version
-    virtual inet_address_vector_replica_set calculate_natural_endpoints_sync(const token& search_token, const token_metadata& tm) const = 0;
-
-protected:
-    inet_address_vector_replica_set do_calculate_natural_endpoints(const token& search_token, const token_metadata& tm, can_yield = can_yield::no) const;
 
 public:
     future<std::unordered_multimap<inet_address, dht::token_range>> get_address_ranges(const token_metadata& tm) const;
