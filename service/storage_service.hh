94d2a196479666a7fc3ee9fecbbf65d4d9f12c83
Benny Halevy <bhalevy@scylladb.com>
2021-11-22 08:28:02 +0200

    storage_service: keep a reference to the batchlog_manager
    Rather than accessing the global batchlog_manager.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -88,6 +88,7 @@ class generation_service;
 
 namespace db {
 class system_distributed_keyspace;
+class batchlog_manager;
 }
 
 namespace netw {
@@ -201,7 +202,8 @@ public:
         sharded<cdc::generation_service>&,
         sharded<repair_service>& repair,
         raft_group_registry& raft_gr,
-        endpoint_lifecycle_notifier& elc_notif);
+        endpoint_lifecycle_notifier& elc_notif,
+        sharded<db::batchlog_manager>& bm);
 
     // Needed by distributed<>
     future<> stop();
@@ -268,6 +270,14 @@ public:
         return *_shared_token_metadata.get();
     }
 
+    sharded<db::batchlog_manager>& get_batchlog_manager() noexcept {
+        return _batchlog_manager;
+    }
+
+    db::batchlog_manager& get_local_batchlog_manager() noexcept {
+        return _batchlog_manager.local();
+    }
+
 private:
     bool is_auto_bootstrap() const;
     inet_address get_broadcast_address() const {
@@ -305,6 +315,7 @@ private:
     /* Used for tracking drain progress */
 
     endpoint_lifecycle_notifier& _lifecycle_notifier;
+    sharded<db::batchlog_manager>& _batchlog_manager;
 
     std::unordered_set<token> _bootstrap_tokens;
 
