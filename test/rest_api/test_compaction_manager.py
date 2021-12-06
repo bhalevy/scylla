50b2e37be89c0cfedbb422280f29b86105abd2b6
Benny Halevy <bhalevy@scylladb.com>
2021-12-06 16:05:36 +0200

    api: compaction_manager: add stop_keyspace_compaction
    Allow stopping compaction by type on a given keyspace
    and list of tables.
    
    Add respective api test.
    
    Fixes #9700
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- test/rest_api/test_compaction_manager.py
+++ test/rest_api/test_compaction_manager.py
@@ -15,6 +15,56 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with Scylla.  If not, see <http://www.gnu.org/licenses/>.
 
+import pytest
+import sys
+import requests
+
+# Use the util.py library from ../cql-pytest:
+sys.path.insert(1, sys.path[0] + '/../cql-pytest')
+from util import new_test_table
+
 def test_compaction_manager_stop_compaction(rest_api):
     resp = rest_api.send("POST", "compaction_manager/stop_compaction", { "type": "COMPACTION" })
+
+def test_compaction_manager_stop_keyspace_compaction(cql, rest_api, test_keyspace):
+    resp = rest_api.send("POST", f"compaction_manager/stop_keyspace_compaction/{test_keyspace}", { "type": "COMPACTION" })
     resp.raise_for_status()
+
+    # non-existing keyspace
+    resp = rest_api.send("POST", f"compaction_manager/stop_keyspace_compaction/XXX", { "type": "COMPACTION" })
+    try:
+        resp.raise_for_status()
+        pytest.fail("Failed to raise exception")
+    except requests.HTTPError as e:
+        expected_status_code = requests.codes.bad_request
+        assert resp.status_code == expected_status_code, e
+
+def test_compaction_manager_stop_keyspace_compaction_tables(cql, rest_api, test_keyspace):
+    with new_test_table(cql, test_keyspace, "a int, PRIMARY KEY (a)") as t0:
+        test_tables = [t0 if not '.' in t0 else t0.split('.')[1]]
+        resp = rest_api.send("POST", f"compaction_manager/stop_keyspace_compaction/{test_keyspace}", { "tables": f"{test_tables[0]}", "type": "COMPACTION" })
+        resp.raise_for_status()
+
+        # non-existing table
+        resp = rest_api.send("POST", f"compaction_manager/stop_keyspace_compaction/{test_keyspace}", { "tables": "XXX", "type": "COMPACTION" })
+        try:
+            resp.raise_for_status()
+            pytest.fail("Failed to raise exception")
+        except requests.HTTPError as e:
+            expected_status_code = requests.codes.bad_request
+            assert resp.status_code == expected_status_code, e
+
+        # multiple tables
+        with new_test_table(cql, test_keyspace, "b int, PRIMARY KEY (b)") as t1:
+            test_tables += [t1 if not '.' in t1 else t1.split('.')[1]]
+            resp = rest_api.send("POST", f"compaction_manager/stop_keyspace_compaction/{test_keyspace}", { "tables": f"{test_tables[0]},{test_tables[1]}", "type": "COMPACTION" })
+            resp.raise_for_status()
+
+            # mixed existing and non-existing tables
+            resp = rest_api.send("POST", f"compaction_manager/stop_keyspace_compaction/{test_keyspace}", { "tables": f"{test_tables[1]},XXX", "type": "COMPACTION" })
+            try:
+                resp.raise_for_status()
+                pytest.fail("Failed to raise exception")
+            except requests.HTTPError as e:
+                expected_status_code = requests.codes.bad_request
+                assert resp.status_code == expected_status_code, e
