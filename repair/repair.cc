9700c47418dbcac097ec149fa6366e7fbaa3491a
Benny Halevy <bhalevy@scylladb.com>
2021-12-13 17:44:47 +0200

    repair: repair_info: keep a reference to the repair_service
    To be used to get to the repair_tracker.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- repair/repair.cc
+++ repair/repair.cc
@@ -582,7 +582,7 @@ get_sharder_for_tables(seastar::sharded<database>& db, const sstring& keyspace, 
     return last_s->get_sharder();
 }
 
-repair_info::repair_info(repair_service& repair,
+repair_info::repair_info(repair_service& repair_,
     const sstring& keyspace_,
     const dht::token_range_vector& ranges_,
     std::vector<utils::UUID> table_ids_,
@@ -592,7 +592,8 @@ repair_info::repair_info(repair_service& repair,
     const std::unordered_set<gms::inet_address>& ignore_nodes_,
     streaming::stream_reason reason_,
     std::optional<utils::UUID> ops_uuid)
-    : db(repair.get_db())
+    : repair(repair_)
+    , db(repair.get_db())
     , messaging(repair.get_messaging().container())
     , sys_dist_ks(repair.get_sys_dist_ks())
     , view_update_generator(repair.get_view_update_generator())
