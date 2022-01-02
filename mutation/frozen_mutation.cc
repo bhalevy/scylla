1870922cce850690c5beac417a51f6e98b12f99d
Benny Halevy <bhalevy@scylladb.com>
2022-01-02 20:44:54 +0200

    frozen_mutation: add freeze_gently_in_thread
    Allow yielding in-between freezing rows.
    
    At the moment yielding between freezing cells
    or tombstones is not supported yet,
    assuming freezing a single row does not stall the reactor
    for too long.
    
    It may be needed to add that in the future
    for supporting large (wide) rows that contain
    many cells, and/or large colletions.
    
    Refs #9866
    
    Also, add variant of freeze_gently_in_thread
    taking a vector of mutations.
    
    The reason we need to run in a thread is
    that ser::writer_of_mutation::partition call
    must be synchronous.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- mutation/frozen_mutation.cc
+++ mutation/frozen_mutation.cc
@@ -133,6 +133,30 @@ frozen_mutation freeze(const mutation& m) {
     return frozen_mutation{ m };
 }
 
+frozen_mutation freeze_gently_in_thread(const mutation& m) {
+    mutation_partition_serializer part_ser(*m.schema(), m.partition());
+
+    bytes_ostream bytes;
+    ser::writer_of_mutation<bytes_ostream> wom(bytes);
+
+    std::move(wom).write_table_id(m.schema()->id())
+                  .write_schema_version(m.schema()->version())
+                  .write_key(m.key())
+                  .partition([&] (auto wr) mutable {
+                    part_ser.write_gently(std::move(wr)).get();
+                  }).end_mutation();
+    return frozen_mutation(std::move(bytes), m.key());
+}
+
+std::vector<frozen_mutation> freeze_gently_in_thread(const std::vector<mutation>& muts) {
+    std::vector<frozen_mutation> result;
+    result.reserve(muts.size());
+    for (const auto& m : muts) {
+        result.emplace_back(freeze_gently_in_thread(m));
+    }
+    return result;
+}
+
 std::vector<frozen_mutation> freeze(const std::vector<mutation>& muts) {
     return boost::copy_range<std::vector<frozen_mutation>>(muts | boost::adaptors::transformed([] (const mutation& m) {
         return freeze(m);
