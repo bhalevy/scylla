8aff50e9a1b2d5335403419dcdccbec53d3e3d05
Benny Halevy <bhalevy@scylladb.com>
2022-02-16 15:29:28 +0200

    test: rest_api: add test_storage_service_keyspace_scrub tests
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- test/rest_api/test_storage_service.py
+++ test/rest_api/test_storage_service.py
@@ -95,3 +95,40 @@ def test_storage_service_keyspace_offstrategy_compaction_tables(cql, this_dc, re
             assert resp.status_code == requests.codes.bad_request
 
     cql.execute(f"DROP KEYSPACE {keyspace}")
+
+def test_storage_service_keyspace_scrub(cql, this_dc, rest_api):
+    keyspace = new_keyspace(cql, this_dc)
+    with new_test_table(cql, keyspace, "a int, PRIMARY KEY (a)") as t0:
+        with new_test_table(cql, keyspace, "a int, PRIMARY KEY (a)") as t1:
+            test_tables = [t0.split('.')[1], t1.split('.')[1]]
+
+            resp = rest_api.send("GET", f"storage_service/keyspace_scrub/{keyspace}")
+            resp.raise_for_status()
+
+            resp = rest_api.send("GET", f"storage_service/keyspace_scrub/{keyspace}", { "cf": f"{test_tables[1]}" })
+            resp.raise_for_status()
+
+            resp = rest_api.send("GET", f"storage_service/keyspace_scrub/{keyspace}", { "cf": f"{test_tables[0]},{test_tables[1]}" })
+            resp.raise_for_status()
+
+            # non-existing table
+            resp = rest_api.send("POST", f"storage_service/keyspace_scrub/{keyspace}", { "cf": f"{test_tables[0]},XXX" })
+            assert resp.status_code == requests.codes.not_found
+
+def test_storage_service_keyspace_scrub_mode(cql, this_dc, rest_api):
+    keyspace = new_keyspace(cql, this_dc)
+    with new_test_table(cql, keyspace, "a int, PRIMARY KEY (a)") as t0:
+        with new_test_table(cql, keyspace, "a int, PRIMARY KEY (a)") as t1:
+            test_tables = [t0.split('.')[1], t1.split('.')[1]]
+
+            resp = rest_api.send("GET", f"storage_service/keyspace_scrub/{keyspace}", { "cf": f"{test_tables[0]}", "scrub_mode": "VALIDATE" })
+            resp.raise_for_status()
+
+            resp = rest_api.send("GET", f"storage_service/keyspace_scrub/{keyspace}", { "cf": f"{test_tables[0]}", "scrub_mode": "XXX" })
+            assert resp.status_code == requests.codes.bad_request
+
+            resp = rest_api.send("GET", f"storage_service/keyspace_scrub/{keyspace}", { "cf": f"{test_tables[0]}", "quarantine_mode": "ONLY" })
+            resp.raise_for_status()
+
+            resp = rest_api.send("GET", f"storage_service/keyspace_scrub/{keyspace}", { "cf": f"{test_tables[0]}", "quarantine_mode": "YYY" })
+            assert resp.status_code == requests.codes.bad_request
