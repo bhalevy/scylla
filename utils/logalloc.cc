d4356768b24ed86bca99fe316f8f67ec43743d6d
Benny Halevy <bhalevy@scylladb.com>
2022-05-29 13:18:51 +0300

    logalloc: segment_pool: delete unused free_or_restore_to_reserve method
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- utils/logalloc.cc
+++ utils/logalloc.cc
@@ -810,7 +810,6 @@ private:
     friend void segment::operator delete(void*);
 
     segment* allocate_or_fallback_to_reserve();
-    void free_or_restore_to_reserve(segment* seg) noexcept;
     const segment* segment_from_idx(size_t idx) const {
         return _store.segment_from_idx(idx);
     }
