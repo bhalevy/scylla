931a6a1279fff51ac96babe3ee2db4afab439e8d
Benny Halevy <bhalevy@scylladb.com>
2022-06-17 17:20:12 +0300

    types: time_point_to_string: harden against out of range timestamps
    The time point is multiplied by an adjustment factor of 1000
    for boost::posix_time::time_duration::ticks_per_second() = 1000000
    when calling boost::posix_time::milliseconds(count)
    and that may lead to integer overflow as reported
    by the UndefinedBehaviorSanitizer.
    
    See https://github.com/scylladb/scylla/issues/10830#issuecomment-1158899187
    
    This change used the fmt library to format the iso representation
    of the time stamp to avoid exceptions and error handling.
    
    Note that time points with large durations will end up
    very far in the future (or in the past if they are negative).
    
    Refs #10830
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- types.cc
+++ types.cc
@@ -27,6 +27,9 @@
 #include <string>
 #include <regex>
 #include <concepts>
+#include <ctime>
+#include <cstdlib>
+#include <fmt/chrono.h>
 #include <boost/iterator/transform_iterator.hpp>
 #include <boost/range/adaptor/filtered.hpp>
 #include <boost/range/numeric.hpp>
@@ -67,14 +70,19 @@ requires requires {
 sstring
 time_point_to_string(const T& tp)
 {
-    int64_t count = tp.time_since_epoch().count();
-    auto time = boost::posix_time::from_time_t(0) + boost::posix_time::milliseconds(count);
-    constexpr std::string_view units = "milliseconds";
-  try {
-    return boost::posix_time::to_iso_extended_string(time);
-  } catch (const std::exception& e) {
-    return format("{} {} ({})", count, units, e.what());
-  }
+    auto d = std::div(int64_t(tp.time_since_epoch().count()), int64_t(1000));
+    std::time_t seconds = d.quot;
+    std::tm tm = *gmtime(&seconds);
+    if (!d.rem) {
+        return fmt::format("{:%Y-%m-%dT%H:%M:%S}", tm);
+    }
+    // adjust seconds for time points earlier than posix epoch
+    if (d.quot < 0) {
+        d.quot--;
+        d.rem = 1000 + d.rem;
+    }
+    uint32_t micros = d.rem * 1000;
+    return fmt::format("{:%Y-%m-%dT%H:%M:%S}.{:06d}", tm, micros);
 }
 
 sstring simple_date_to_string(const uint32_t days_count) {
