4a99dbfb42fe9cc94d0744769e8a7ad0b15e687d
Benny Halevy <bhalevy@scylladb.com>
2022-07-03 18:36:50 +0300

    table: try_flush_memtable_to_sstable: handle exceptions from maybe_wait_for_sstable_count_reduction
    We must close the reader if co_await'ing
    maybe_wait_for_sstable_count_reduction throws.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- replica/table.cc
+++ replica/table.cc
@@ -689,16 +689,18 @@ table::try_flush_memtable_to_sstable(lw_shared_ptr<memtable> old, sstable_write_
                 _memtables->erase(old);
                 co_return stop_iteration::yes;
             }
+            co_await maybe_wait_for_sstable_count_reduction();
         } catch (...) {
             err = std::current_exception();
         }
         if (err) {
+            // TODO: allow silently breaking _sstables_changed on stop()
+            // to wake maybe_wait_for_sstable_count_reduction up
             tlogger.error("failed to flush memtable for {}.{}: {}", old->schema()->ks_name(), old->schema()->cf_name(), err);
             co_await reader.close();
             co_return stop_iteration(_async_gate.is_closed());
         }
 
-        co_await maybe_wait_for_sstable_count_reduction();
         auto f = consumer(std::move(reader));
 
         // Switch back to default scheduling group for post-flush actions, to avoid them being staved by the memtable flush
