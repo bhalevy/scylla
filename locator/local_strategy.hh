690bce606e66e060c9ed9a4d28b6732549215c10
Benny Halevy <bhalevy@scylladb.com>
2022-07-10 18:26:48 +0300

    abstract_replication_strategy: add maybe_calculate_natural_endpoints
    Call the strategy's calculate_natural_endpoints()
    only once it has_static_natural_endpoints()
    (like local_strategy or everywhere_replication_strategy)
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- locator/local_strategy.hh
+++ locator/local_strategy.hh
@@ -27,6 +27,8 @@ public:
     virtual ~local_strategy() {};
     virtual size_t get_replication_factor(const token_metadata&) const override;
 
+    virtual bool has_static_natural_endpoints() const noexcept override { return true; }
+
     virtual future<inet_address_vector_replica_set> calculate_natural_endpoints(const token& search_token, const token_metadata& tm) const override;
 
     virtual void validate_options() const override;
