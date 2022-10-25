0fa3aab5480c3307afd25d39ab7ad659dcf55b13
Benny Halevy <bhalevy@scylladb.com>
2022-10-25 21:27:33 +0300

    utils: timeuuid_cmp: turn into a class
    Make the internal functions private.
    In preparation for adding support for time uuid v7.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- utils/UUID_cmp.hh
+++ utils/UUID_cmp.hh
@@ -17,8 +17,28 @@
 
 namespace utils {
 
+class timeuuid_cmp {
+    bytes_view _o1;
+    bytes_view _o2;
+
+private:
+    static inline std::strong_ordering uint64_t_tri_compare(uint64_t a, uint64_t b) noexcept;
+    static inline uint64_t timeuuid_read_msb(const int8_t* b) noexcept;
+    static inline uint64_t uuid_read_lsb(const int8_t* b) noexcept;
+
+public:
+    timeuuid_cmp(bytes_view o1, bytes_view o2) noexcept : _o1(o1), _o2(o2) {}
+
+    inline std::strong_ordering tri_compare() const noexcept;
+    inline std::strong_ordering uuid_tri_compare() const noexcept;
+};
+
+inline std::strong_ordering timeuuid_cmp::uint64_t_tri_compare(uint64_t a, uint64_t b) noexcept {
+    return a <=> b;
+}
+
 // Read 8 most significant bytes of timeuuid from serialized bytes
-inline uint64_t timeuuid_read_msb(const int8_t *b) noexcept {
+inline uint64_t timeuuid_cmp::timeuuid_read_msb(const int8_t* b) noexcept {
     // cast to unsigned to avoid sign-compliment during shift.
     auto u64 = [](uint8_t i) -> uint64_t { return i; };
     // Scylla and Cassandra use a standard UUID memory layout for MSB:
@@ -33,7 +53,7 @@ inline uint64_t timeuuid_read_msb(const int8_t *b) noexcept {
            u64(b[2]) << 8  | u64(b[3]);
 }
 
-inline uint64_t uuid_read_lsb(const int8_t *b) noexcept {
+inline uint64_t timeuuid_cmp::uuid_read_lsb(const int8_t* b) noexcept {
     auto u64 = [](uint8_t i) -> uint64_t { return i; };
     return u64(b[8]) << 56 | u64(b[9]) << 48 |
            u64(b[10]) << 40 | u64(b[11]) << 32 |
@@ -50,10 +70,12 @@ inline uint64_t uuid_read_lsb(const int8_t *b) noexcept {
 // To avoid breaking ordering in existing sstables, Scylla preserves
 // Cassandra compare order.
 //
-inline std::strong_ordering timeuuid_tri_compare(const int8_t* o1, const int8_t* o2) noexcept {
+inline std::strong_ordering timeuuid_cmp::tri_compare() const noexcept {
     auto timeuuid_read_lsb = [](const int8_t* o) -> uint64_t {
         return uuid_read_lsb(o) ^ 0x8080808080808080;
     };
+    const int8_t* o1 = _o1.begin();
+    const int8_t* o2 = _o2.begin();
     auto res = timeuuid_read_msb(o1) <=> timeuuid_read_msb(o2);
     if (res == 0) {
         res = timeuuid_read_lsb(o1) <=> timeuuid_read_lsb(o2);
@@ -62,7 +84,7 @@ inline std::strong_ordering timeuuid_tri_compare(const int8_t* o1, const int8_t*
 }
 
 inline std::strong_ordering timeuuid_tri_compare(bytes_view o1, bytes_view o2) noexcept {
-    return timeuuid_tri_compare(o1.begin(), o2.begin());
+    return timeuuid_cmp(o1.begin(), o2.begin()).tri_compare();
 }
 
 inline std::strong_ordering timeuuid_tri_compare(const UUID& u1, const UUID& u2) noexcept {
@@ -86,10 +108,10 @@ inline std::strong_ordering timeuuid_tri_compare(const UUID& u1, const UUID& u2)
 // which is both faster and monotonic, so should be preferred
 // to @timeuuid_tri_compare() used for all new features.
 //
-inline std::strong_ordering uuid_tri_compare_timeuuid(bytes_view o1, bytes_view o2) noexcept {
-    auto res = timeuuid_read_msb(o1.begin()) <=> timeuuid_read_msb(o2.begin());
+inline std::strong_ordering timeuuid_cmp::uuid_tri_compare() const noexcept {
+    auto res = timeuuid_read_msb(_o1.begin()) <=> timeuuid_read_msb(_o2.begin());
     if (res == 0) {
-        res = uuid_read_lsb(o1.begin()) <=> uuid_read_lsb(o2.begin());
+        res = uuid_read_lsb(_o1.begin()) <=> uuid_read_lsb(_o2.begin());
     }
     return res;
 }
