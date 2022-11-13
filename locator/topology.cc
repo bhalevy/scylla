02f66b138cae6139befa5ae37adc4a195f393c86
Benny Halevy <bhalevy@scylladb.com>
2022-11-13 20:18:39 +0200

    topology: add class node
    And keep per node information (host_id, endpoint, dc_rack, is_pending)
    in node objects, indexed on several indices like:
    host_id, endpoint, current/pending, per dc, per dc/rack.
    
    Refs #6403
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- locator/topology.cc
+++ locator/topology.cc
@@ -25,22 +25,71 @@ thread_local const endpoint_dc_rack endpoint_dc_rack::default_location = {
     .rack = locator::production_snitch_base::default_rack,
 };
 
+topology::node::node(::locator::host_id id, inet_address endpoint, endpoint_dc_rack dc_rack, local is_local)
+    : _host_id(id)
+    , _endpoint(endpoint)
+    , _dc_rack(std::move(dc_rack))
+    , _is_local(is_local)
+{}
+
+void topology::node::unlink() noexcept {
+    _dc_nodes_link.unlink();
+    _rack_nodes_link.unlink();
+    _nodes_link.unlink();
+}
+
 future<> topology::clear_gently() noexcept {
     co_await utils::clear_gently(_dc_endpoints);
     co_await utils::clear_gently(_dc_racks);
-    co_await utils::clear_gently(_current_locations);
     _datacenters.clear();
+    _nodes_by_endpoint.clear();
+    _nodes_by_host_id.clear();
+    co_await utils::clear_gently(_all_nodes);
     co_return;
+}
+
+topology::topology() noexcept
+{
+    tlogger.trace("topology[{}]: default-constructed", fmt::ptr(this));
 }
 
 topology::topology(config cfg)
         : _sort_by_proximity(!cfg.disable_proximity_sorting)
 {
-    update_endpoint(utils::fb_utilities::get_broadcast_address(), cfg.local_dc_rack);
+    tlogger.trace("topology[{}]: constructing using config: host_id={} endpoint={} dc={} rack={}", fmt::ptr(this),
+            cfg.local_host_id, cfg.local_endpoint, cfg.local_dc_rack.dc, cfg.local_dc_rack.rack);
+    if (cfg.local_host_id || cfg.local_endpoint != inet_address{}) {
+        add_node(make_lw_shared<node>(cfg.local_host_id, cfg.local_endpoint, cfg.local_dc_rack, node::local::yes));
+    }
+}
+
+topology::topology(topology&& o) noexcept
+    : _all_nodes(std::move(o._all_nodes))
+    , _local_node(std::exchange(o._local_node, nullptr))
+    , _nodes_by_host_id(std::move(o._nodes_by_host_id))
+    , _nodes_by_endpoint(std::move(o._nodes_by_endpoint))
+    , _dc_nodes(std::move(o._dc_nodes))
+    , _dc_rack_nodes(std::move(o._dc_rack_nodes))
+    , _current_nodes(std::move(o._current_nodes))
+    , _dc_endpoints(std::move(o._dc_endpoints))
+    , _dc_racks(std::move(o._dc_racks))
+    , _sort_by_proximity(o._sort_by_proximity)
+    , _datacenters(std::move(o._datacenters))
+{
+    tlogger.trace("topology[{}]: move from [{}]", fmt::ptr(this), fmt::ptr(&o));
 }
 
 future<topology> topology::clone_gently() const {
     topology ret;
+    for (const auto& n : _all_nodes) {
+        ret.add_node(make_lw_shared<node>(*n));
+        co_await coroutine::maybe_yield();
+    }
+    // local node may be detached for _all_nodes
+    // if it was decommissioned.
+    if (!ret._local_node && _local_node) {
+        ret._local_node = make_lw_shared<node>(*_local_node);
+    }
     ret._dc_endpoints.reserve(_dc_endpoints.size());
     for (const auto& p : _dc_endpoints) {
         ret._dc_endpoints.emplace(p);
@@ -54,46 +103,173 @@ future<topology> topology::clone_gently() const {
         }
     }
     co_await coroutine::maybe_yield();
-    ret._current_locations.reserve(_current_locations.size());
-    for (const auto& p : _current_locations) {
-        ret._current_locations.emplace(p);
-    }
-    co_await coroutine::maybe_yield();
     ret._datacenters = _datacenters;
     ret._sort_by_proximity = _sort_by_proximity;
     co_return ret;
 }
 
-void topology::update_endpoint(const inet_address& ep, endpoint_dc_rack dr)
-{
-    auto current = _current_locations.find(ep);
-
-    if (current != _current_locations.end()) {
-        if (current->second.dc == dr.dc && current->second.rack == dr.rack) {
-            return;
-        }
-        remove_endpoint(ep);
+void topology::add_node(host_id id, const inet_address& ep, const endpoint_dc_rack& dr) {
+    if (dr.dc.empty()) {
+        on_internal_error(tlogger, "Node must have a valid dc");
     }
-
-    tlogger.debug("update_endpoint: {} {}/{}", ep, dr.dc, dr.rack);
-    _dc_endpoints[dr.dc].insert(ep);
-    _dc_racks[dr.dc][dr.rack].insert(ep);
-    _datacenters.insert(dr.dc);
-    _current_locations[ep] = std::move(dr);
+    add_node(make_lw_shared<node>(id, ep, dr, node::local(ep == utils::fb_utilities::get_broadcast_address())));
 }
 
-void topology::remove_endpoint(inet_address ep)
-{
-    auto cur_dc_rack = _current_locations.find(ep);
+void topology::add_node(lw_shared_ptr<node> node) {
+    tlogger.debug("topology[{}]: add_node: node={} host_id={} endpoint={} dc={} rack={} local={}, at {}", fmt::ptr(this), fmt::ptr(node.get()),
+            node->host_id(), node->endpoint(), node->dc_rack().dc, node->dc_rack().rack, node->is_local(), current_backtrace());
+    if (node->endpoint() == inet_address{}) {
+        on_internal_error(tlogger, "Node must have a valid endpoint");
+    }
+    if (node->is_local() && _local_node && _local_node != node) {
+        on_internal_error(tlogger, format("Local node already set: host_id={} endpoint={} dc={} rack={}: currently mapped to host_id={} endpoint={}",
+                node->host_id(), node->endpoint(), node->dc_rack().dc, node->dc_rack().rack,
+                _local_node->host_id(), _local_node->endpoint()));
+    }
+    if (_all_nodes.contains(node)) {
+        return;
+    }
+    try {
+        // FIXME: for now we allow adding nodes with null host_id
+        if (node->host_id()) {
+            auto [nit, inserted_host_id] = _nodes_by_host_id.emplace(node->host_id(), node.get());
+            if (!inserted_host_id) {
+                on_internal_error(tlogger, format("Node already exists: host_id={} endpoint={} dc={} rack={}",
+                        node->host_id(), node->endpoint(), node->dc_rack().dc, node->dc_rack().rack));
+            }
+        }
+        if (node->endpoint() != inet_address{}) {
+            auto [eit, inserted_endpoint] = _nodes_by_endpoint.emplace(node->endpoint(), node.get());
+            if (!inserted_endpoint) {
+                if (node->host_id()) {
+                    _nodes_by_host_id.erase(node->host_id());
+                }
+                on_internal_error(tlogger, format("Node endpoint already mapped: host_id={} endpoint={} dc={} rack={}: currently mapped to host_id={}",
+                        node->host_id(), node->endpoint(), node->dc_rack().dc, node->dc_rack().rack,
+                        eit->second->host_id()));
+            }
+        }
 
-    if (cur_dc_rack == _current_locations.end()) {
+        _current_nodes.push_back(*node);
+
+        const auto& dc = node->dc_rack().dc;
+        const auto& rack = node->dc_rack().rack;
+        const auto& endpoint = node->endpoint();
+        _dc_nodes[dc].push_back(*node);
+        _dc_rack_nodes[dc][rack].push_back(*node);
+        _dc_endpoints[dc].insert(endpoint);
+        _dc_racks[dc][rack].insert(endpoint);
+        _datacenters.insert(dc);
+
+        if (node->is_local()) {
+            _local_node = node;
+        }
+        _all_nodes.emplace(std::move(node));
+    } catch (...) {
+        remove_node(node);
+        throw;
+    }
+}
+
+void topology::update_node(lw_shared_ptr<node> node, std::optional<host_id> opt_id, std::optional<inet_address> opt_ep, std::optional<endpoint_dc_rack> opt_dr) {
+    tlogger.debug("topology[{}]: update_node: node={} host_id={} endpoint={} dc={} rack={}, at {}", fmt::ptr(this), fmt::ptr(node.get()),
+            opt_id.value_or(host_id::create_null_id()), opt_ep.value_or(inet_address{}), opt_dr.value_or(endpoint_dc_rack{}).dc, opt_dr.value_or(endpoint_dc_rack{}).rack,
+            current_backtrace());
+    bool changed = false;
+    if (opt_id) {
+        if (*opt_id != node->host_id()) {
+            if (node->host_id()) {
+                on_internal_error(tlogger, format("Updating non-null node host_id is disallowed: host_id={} endpoint={}: new host_id={}",
+                        node->host_id(), node->endpoint(), *opt_id));
+            }
+            if (!*opt_id) {
+                on_internal_error(tlogger, format("Updating node host_id to null is disallowed: host_id={} endpoint={}: new host_id={}",
+                        node->host_id(), node->endpoint(), *opt_id));
+            }
+            if (_nodes_by_host_id.contains(*opt_id)) {
+                on_internal_error(tlogger, format("Cannot update node host_id: new host_id={} already exists: endpoint={}: ",
+                        *opt_id, node->endpoint()));
+            }
+            changed = true;
+        } else {
+            opt_id.reset();
+        }
+    }
+    if (opt_ep) {
+        if (*opt_ep != node->endpoint()) {
+            if (*opt_ep == inet_address{}) {
+                on_internal_error(tlogger, format("Updating node endpoint to null is disallowed: host_id={} endpoint={}: new endpoint={}",
+                        node->host_id(), node->endpoint(), *opt_ep));
+            }
+            changed = true;
+        } else {
+            opt_ep.reset();
+        }
+    }
+    if (opt_dr) {
+        if (opt_dr->dc.empty() || opt_dr->dc == production_snitch_base::default_dc) {
+            opt_dr->dc = node->dc_rack().dc;
+        }
+        if (opt_dr->rack.empty() || opt_dr->rack == production_snitch_base::default_rack) {
+            opt_dr->rack = node->dc_rack().rack;
+        }
+        if (*opt_dr != node->dc_rack()) {
+            changed = true;
+        } else {
+            opt_dr.reset();
+        }
+    }
+
+    if (!changed) {
         return;
     }
 
-    const auto& dc = cur_dc_rack->second.dc;
-    const auto& rack = cur_dc_rack->second.rack;
-    tlogger.debug("remove_endpoint: {} {}/{}", ep, dc, rack);
+    remove_node(node);
+    if (opt_id) {
+        node->_host_id = *opt_id;
+    }
+    if (opt_ep) {
+        node->_endpoint = *opt_ep;
+    }
+    if (opt_dr) {
+        node->_dc_rack = std::move(*opt_dr);
+    }
+    add_node(node);
+}
+
+void topology::remove_node(host_id id, must_exist require_exist) {
+    if (id == _local_node->host_id()) {
+        on_internal_error(tlogger, format("Cannot remove the local node: host_id={} endpoint={}",
+                _local_node->host_id(), _local_node->endpoint()));
+    }
+    tlogger.trace("topology[{}]: remove_node: host_id={}", fmt::ptr(this), id);
+    auto it = _nodes_by_host_id.find(id);
+    if (it != _nodes_by_host_id.end()) {
+        remove_node(it->second);
+    } else if (require_exist) {
+        on_internal_error(tlogger, format("Node not found: host_id={}", id));
+    }
+}
+
+void topology::remove_node(node* np) {
+    if (np) {
+        do_remove_node(np->shared_from_this());
+    }
+}
+
+void topology::remove_node(node_ptr np) {
+    if (np) {
+        do_remove_node(const_cast<node*>(np.get())->shared_from_this());
+    }
+}
+
+void topology::do_remove_node(lw_shared_ptr<node> node) {
+    tlogger.debug("remove_node: node={} host_id={} endpoint={}, at {}", fmt::ptr(node.get()), node->host_id(), node->endpoint(), current_backtrace());
+ 
+    const auto& dc = node->dc_rack().dc;
+    const auto& rack = node->dc_rack().rack;
     if (auto dit = _dc_endpoints.find(dc); dit != _dc_endpoints.end()) {
+        const auto& ep = node->endpoint();
         auto& eps = dit->second;
         eps.erase(ep);
         if (eps.empty()) {
@@ -112,50 +288,88 @@ void topology::remove_endpoint(inet_address ep)
         }
     }
 
-    // Keep the local endpoint around
-    // Just unlist it from _dc_endpoints and _dc_racks
-    // This is needed after it is decommissioned
-    if (ep != utils::fb_utilities::get_broadcast_address()) {
-        _current_locations.erase(cur_dc_rack);
+    _nodes_by_host_id.erase(node->host_id());
+    _nodes_by_endpoint.erase(node->endpoint());
+    node->unlink();
+    _all_nodes.erase(node);
+}
+
+// Finds a node by its host_id
+// Returns nullptr if not found
+topology::node_ptr topology::find_node(host_id id, must_exist must_exist) const noexcept {
+    auto it = _nodes_by_host_id.find(id);
+    if (it != _nodes_by_host_id.end()) {
+        return it->second->shared_from_this();
     }
+    if (must_exist) {
+        on_internal_error(tlogger, format("Could not find node: host_id={}", id));
+    }
+    return nullptr;
+}
+
+// Finds a node by its endpoint
+// Returns nullptr if not found
+topology::node_ptr topology::find_node(const inet_address& ep, must_exist must_exist) const noexcept {
+    auto it = _nodes_by_endpoint.find(ep);
+    if (it != _nodes_by_endpoint.end()) {
+        return it->second->shared_from_this();
+    }
+    if (must_exist) {
+        on_internal_error(tlogger, format("Could not find node: endpoint={}", ep));
+    }
+    return nullptr;
+}
+
+void topology::update_endpoint(inet_address ep, std::optional<host_id> opt_id, std::optional<endpoint_dc_rack> opt_dr)
+{
+    tlogger.trace("topology[{}]: update_endpoint: ep={} host_id={} dc={} rack={}, at {}", fmt::ptr(this),
+            ep, opt_id.value_or(host_id::create_null_id()), opt_dr.value_or(endpoint_dc_rack{}).dc, opt_dr.value_or(endpoint_dc_rack{}).rack,
+            current_backtrace());
+    node_ptr n = find_node(ep);
+    if (n) {
+        update_node(const_cast<node*>(n.get())->shared_from_this(), opt_id, std::nullopt, std::move(opt_dr));
+    } else if (opt_id && (n = find_node(*opt_id))) {
+        update_node(const_cast<node*>(n.get())->shared_from_this(), std::nullopt, ep, std::move(opt_dr));
+    } else {
+        add_node(opt_id.value_or(host_id::create_null_id()), ep, opt_dr.value_or(endpoint_dc_rack::default_location));
+    }
+}
+
+void topology::remove_endpoint(inet_address ep)
+{
+    tlogger.trace("topology[{}]: remove_endpoint: endpoint={}", fmt::ptr(this), ep);
+    remove_node(find_node(ep));
+}
+
+bool topology::has_node(host_id id) const noexcept {
+    auto node = find_node(id);
+    tlogger.trace("topology[{}]: has_node: host_id={}: node={}", fmt::ptr(this), id, fmt::ptr(node.get()));
+    return bool(node);
+}
+
+bool topology::has_node(inet_address ep) const noexcept {
+    auto node = find_node(ep);
+    tlogger.trace("topology[{}]: has_node: endpoint={}: node={}", fmt::ptr(this), ep, fmt::ptr(node.get()));
+    return bool(node);
 }
 
 bool topology::has_endpoint(inet_address ep) const
 {
-    return _current_locations.contains(ep);
+    return has_node(ep);
 }
 
 const endpoint_dc_rack& topology::get_location(const inet_address& ep) const {
-    if (_current_locations.contains(ep)) {
-        return _current_locations.at(ep);
+    if (ep == utils::fb_utilities::get_broadcast_address()) {
+        return get_location();
     }
-
+    if (auto node = find_node(ep, must_exist::no)) {
+        return node->dc_rack();
+    }
     // FIXME -- this shouldn't happen. After topology is stable and is
     // correctly populated with endpoints, this should be replaced with
     // on_internal_error()
     tlogger.warn("Requested location for node {} not in topology. backtrace {}", ep, current_backtrace());
     return endpoint_dc_rack::default_location;
-}
-
-// FIXME -- both methods below should rather return data from the
-// get_location() result, but to make it work two things are to be fixed:
-// - topology should be aware of internal-ip conversions
-// - topology should be pre-populated with data loaded from system ks
-
-sstring topology::get_rack() const {
-    return get_rack(utils::fb_utilities::get_broadcast_address());
-}
-
-sstring topology::get_rack(inet_address ep) const {
-    return get_location(ep).rack;
-}
-
-sstring topology::get_datacenter() const {
-    return get_datacenter(utils::fb_utilities::get_broadcast_address());
-}
-
-sstring topology::get_datacenter(inet_address ep) const {
-    return get_location(ep).dc;
 }
 
 void topology::sort_by_proximity(inet_address address, inet_address_vector_replica_set& addresses) const {
