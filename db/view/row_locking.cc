332495b14b204bef3a89d182f91982055f83a574
Benny Halevy <bhalevy@scylladb.com>
2022-12-04 16:28:37 +0200

    view: row_lock: lock_ck: serialize partition and row locking
    The problematic scenario this patch fixes might happen due to
    unfortunate serialization of locks/unlocks between lock_pk and lock_ck,
    as follows:
    
        1. lock_pk acquires an exclusive lock on the partition.
        2.a lock_ck attempts to acquire shared lock on the partition
            and any lock on the row. both cases currently use a fiber
            returning a future<rwlock::holder>.
        2.b since the partition is locked, the lock_partition times out
            returning an exceptional future.  lock_row has no such problem
            and succeeds, returning a future holding a rwlock::holder,
            pointing to the row lock.
        3.a the lock_holder previously returned by lock_pk is destroyed,
            calling `row_locker::unlock`
        3.b row_locker::unlock sees that the partition is not locked
            and erases it, including the row locks it contains.
        4.a when_all_succeeds continuation in lock_ck runs.  Since
            the lock_partition future failed, it destroyes both futures.
        4.b the lock_row future is destroyed with the rwlock::holder value.
        4.c ~holder attempts to return the semaphore units to the row rwlock,
            but the latter was already destroyed in 3.b above.
    
    Acquiring the partition lock and row lock in parallel
    doesn't help anything, but it complicates error handling
    as seen above,
    
    This patch turns both lock_pk and lock_ck into coroutines.
    lock_ck first awaits the partition lock,
    then it awaits the row lock.
    This way, erasing the unlocked partition is never expected
    to happen while any of its locks is held.
    
    Fixes #12168
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- db/view/row_locking.cc
+++ db/view/row_locking.cc
@@ -10,6 +10,7 @@
 #include "log.hh"
 
 #include <seastar/core/when_all.hh>
+#include <seastar/core/coroutine.hh>
 
 static logging::logger mylog("row_locking");
 
@@ -74,14 +75,12 @@ row_locker::lock_pk(const dht::decorated_key& pk, bool exclusive, db::timeout_cl
     auto tracker = latency_stats_tracker(exclusive ? stats.exclusive_partition : stats.shared_partition);
     auto i = _two_level_locks.try_emplace(pk, this).first;
     auto f = exclusive ? i->second._partition_lock.write_lock(timeout) : i->second._partition_lock.read_lock(timeout);
+    co_await std::move(f);
     // Note: we rely on the fact that &i->first, the pointer to a key, never
     // becomes invalid (as long as the item is actually in the hash table),
     // even in the case of rehashing.
-    return f.then([this, pk = &i->first, exclusive, tracker = std::move(tracker)] () mutable {
-    // FIXME: indentation broken on purpose
     tracker.lock_acquired();
-        return lock_holder(this, pk, exclusive);
-    });
+    co_return lock_holder(this, &i->first, exclusive);
 }
 
 future<row_locker::lock_holder>
@@ -89,34 +88,18 @@ row_locker::lock_ck(const dht::decorated_key& pk, const clustering_key_prefix& c
     mylog.debug("taking shared lock on partition {}, and {} lock on row {} in it", pk, (exclusive ? "exclusive" : "shared"), cpk);
     auto tracker = latency_stats_tracker(exclusive ? stats.exclusive_row : stats.shared_row);
     auto i = _two_level_locks.try_emplace(pk, this).first;
-    future<lock_type::holder> lock_partition = i->second._partition_lock.hold_read_lock(timeout);
+    auto partition_lock_holder = co_await i->second._partition_lock.hold_read_lock(timeout);
     auto j = i->second._row_locks.find(cpk);
     if (j == i->second._row_locks.end()) {
         // Not yet locked, need to create the lock. This makes a copy of cpk.
-        try {
+        // FIXME: indentation
             j = i->second._row_locks.emplace(cpk, lock_type()).first;
-        } catch(...) {
-            // If this emplace() failed, e.g., out of memory, we fail. We
-            // could do nothing - the partition lock we already started
-            // taking will be unlocked automatically after being locked.
-            // But it's better form to wait for the work we started, and it
-            // will also allow us to remove the hash-table row we added.
-            return lock_partition.then([ex = std::current_exception()] (auto lock) {
-                // The lock is automatically released when "lock" goes out of scope.
-                // TODO: unlock (lock = {}) now, search for the partition in the
-                // hash table (we know it's still there, because we held the lock until
-                // now) and remove the unused lock from the hash table if still unused.
-                return make_exception_future<row_locker::lock_holder>(std::current_exception());
-            });
-        }
     }
-    future<lock_type::holder> lock_row = exclusive ? j->second.hold_write_lock(timeout) : j->second.hold_read_lock(timeout);
-    return when_all_succeed(std::move(lock_partition), std::move(lock_row))
-    .then_unpack([this, pk = &i->first, cpk = &j->first, exclusive, tracker = std::move(tracker)] (auto lock1, auto lock2) mutable {
-    // FIXME: indentation broken on purpose
+    auto row_lock_holder = co_await (exclusive ? j->second.hold_write_lock(timeout) : j->second.hold_read_lock(timeout));
     tracker.lock_acquired();
-        return lock_holder(this, pk, cpk, exclusive);
-    });
+    partition_lock_holder.release();
+    row_lock_holder.release();
+    co_return lock_holder(this, &i->first, &j->first, exclusive);
 }
 
 row_locker::lock_holder::lock_holder(row_locker::lock_holder&& old) noexcept
