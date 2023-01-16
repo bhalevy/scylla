c99e1105001c78d1cd266ba4ca33144632eb303e
Benny Halevy <bhalevy@scylladb.com>
2023-01-16 18:55:13 +0200

    message: msg_addr: add host_id
    So we can associate the inet_address with a unique host_id.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- message/msg_addr.hh
+++ message/msg_addr.hh
@@ -10,16 +10,19 @@
 
 #include "gms/inet_address.hh"
 #include <cstdint>
+#include "locator/host_id.hh"
 
 namespace netw {
 
 struct msg_addr {
+    locator::host_id host_id;
     gms::inet_address addr;
     uint32_t cpu_id;
     friend bool operator==(const msg_addr& x, const msg_addr& y) noexcept;
     friend bool operator<(const msg_addr& x, const msg_addr& y) noexcept;
-    explicit msg_addr(gms::inet_address ip) noexcept : addr(ip), cpu_id(0) { }
-    msg_addr(gms::inet_address ip, uint32_t cpu) noexcept : addr(ip), cpu_id(cpu) { }
+    explicit msg_addr(gms::inet_address ip) noexcept : host_id(locator::host_id::create_null_id()), addr(ip), cpu_id(0) { }
+    msg_addr(gms::inet_address ip, uint32_t cpu) noexcept : host_id(locator::host_id::create_null_id()), addr(ip), cpu_id(cpu) { }
+    msg_addr(const locator::host_id& id, gms::inet_address ip, uint32_t cpu = 0) noexcept : host_id(id), addr(ip), cpu_id(cpu) { }
 };
 
 } // namespace netw
@@ -34,7 +37,7 @@ struct hash<netw::msg_addr> {
 };
 
 inline std::ostream& operator<<(std::ostream& os, const netw::msg_addr& id) {
-    return os << id.addr << ':' << id.cpu_id;
+    return os << id.host_id << '/' << id.addr << ':' << id.cpu_id;
 }
 
 } // namespace std
