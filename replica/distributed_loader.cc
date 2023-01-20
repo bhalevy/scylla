a7e54da093041de5ed7f7dd629f74b8322cad5f5
Benny Halevy <bhalevy@scylladb.com>
2023-01-20 15:09:26 +0200

    compaction_manager: needs_cleanup: delete unused schema param
    It isn't needed.  The sstable already has a schema.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- replica/distributed_loader.cc
+++ replica/distributed_loader.cc
@@ -166,7 +166,7 @@ collect_sstables_for_reshard(sharded<sstables::sstable_directory>& dir, compacti
         if (owned_ranges_ptr) {
             const auto& owned_ranges = *owned_ranges_ptr;
             co_await d.do_for_each_sstable([&] (sstables::shared_sstable sst) -> future<> {
-                if (needs_cleanup(sst, owned_ranges, sst->get_schema())) {
+                if (needs_cleanup(sst, owned_ranges)) {
                     need_cleanup.push_back(co_await sst->get_open_info());
                 }
             });
