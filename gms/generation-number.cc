037a0b170f7e2f605ed92a9c495f6a788a10e596
Benny Halevy <bhalevy@scylladb.com>
2023-02-22 11:37:14 +0200

    gossiper: version_generator: add {debug_,}validate_gossip_generation
    Make sure that the int64_t generation we get over rpc
    fits in the int32_t generation_type we keep locally.
    
    Restrict this assertion to non-release builds.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- gms/generation-number.cc
+++ gms/generation-number.cc
@@ -8,6 +8,10 @@
 
 #include <cassert>
 #include <chrono>
+#include <exception>
+
+#include <fmt/format.h>
+
 #include "generation-number.hh"
 
 namespace gms {
@@ -20,6 +24,13 @@ generation_type get_generation_number() {
     // Make sure the clock didn't overflow the 32 bits value
     assert(ret.value() == generation_number);
     return ret;
+}
+
+void validate_gossip_generation(int64_t generation_number) {
+    if (generation_number > std::numeric_limits<gms::generation_type::value_type>::max() ||
+        generation_number < std::numeric_limits<gms::generation_type::value_type>::min()) {
+        throw std::out_of_range(fmt::format("gossip generation {} is out of range", generation_number));
+    }
 }
 
 }
