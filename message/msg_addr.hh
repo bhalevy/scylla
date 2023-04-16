7086e9b89ec9ecb09df902e32517944c44498952
Benny Halevy <bhalevy@scylladb.com>
2023-04-16 18:55:54 +0300

    netw: msg_addr: move hash function to namespace std
    So it can be used by default by respective stl containers.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- message/msg_addr.hh
+++ message/msg_addr.hh
@@ -19,9 +19,7 @@ struct msg_addr {
     friend bool operator==(const msg_addr& x, const msg_addr& y) noexcept;
     friend bool operator<(const msg_addr& x, const msg_addr& y) noexcept;
     friend std::ostream& operator<<(std::ostream& os, const msg_addr& x);
-    struct hash {
-        size_t operator()(const msg_addr& id) const noexcept;
-    };
+
     explicit msg_addr(gms::inet_address ip) noexcept : addr(ip), cpu_id(0) { }
     msg_addr(gms::inet_address ip, uint32_t cpu) noexcept : addr(ip), cpu_id(cpu) { }
 };
@@ -43,5 +41,13 @@ struct formatter<netw::msg_addr> : formatter<std::string_view> {
 namespace std {
 
 std::ostream& operator<<(std::ostream& os, const netw::msg_addr& x);
+
+template <>
+struct hash<netw::msg_addr> {
+    size_t operator()(const netw::msg_addr& id) const noexcept {
+        // Ignore cpu id for now since we do not really support // shard to shard connections
+        return std::hash<bytes_view>()(id.addr.bytes());
+    }
+};
 
 } // namespace std
