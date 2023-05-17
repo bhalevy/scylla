8dd365b9354eafda9b48fceee0dcb5ccc4a24ed5
Benny Halevy <bhalevy@scylladb.com>
2023-05-17 19:16:45 +0300

    gms: endpoint_state_map: set initial HOST_ID and RPC_ADDRESS
    When inserting endpoint_state into the map
    add their HOST_ID / RPC_ADDRESS application states
    if needed.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- gms/endpoint_state.cc
+++ gms/endpoint_state.cc
@@ -15,6 +15,7 @@
 #include "gms/application_state.hh"
 #include "gms/endpoint_state.hh"
 #include "gms/versioned_value.hh"
+#include "seastar/core/on_internal_error.hh"
 #include <optional>
 #include <ostream>
 
@@ -114,9 +115,14 @@ endpoint_state& endpoint_state_map::get_or_create(const endpoint_id& node) {
     if (!node.host_id || node.addr == inet_address()) {
         on_internal_error(logger, format("endpoint_state_map::get_or_create: invalid endpoint {}", node));
     }
+    if (node.addr == inet_address()) {
+        on_internal_error(logger, format("Cannot get_or_create state for endpoint {} with null address", node.host_id));
+    }
     auto it = _state_by_address.find(node.addr);
     if (it == _state_by_address.end()) {
         auto eps = endpoint_state();
+        eps.add_application_state(application_state::HOST_ID, versioned_value::host_id(node.host_id));
+        eps.add_application_state(application_state::RPC_ADDRESS, versioned_value::rpcaddress(node.addr));
         it = _state_by_address.emplace(node.addr, std::move(eps)).first;
     }
     return it->second;
@@ -131,12 +137,16 @@ endpoint_state& endpoint_state_map::set(const endpoint_id& node, endpoint_state&
         if (state_host_id != node.host_id) {
             on_internal_error(logger, format("endpoint_state_map::set: node {} has miamatching host_id={} in endpoint_state", node, state_host_id));
         }
+    } else {
+        eps.add_application_state(application_state::HOST_ID, versioned_value::host_id(node.host_id));
     }
     if (auto addr_state = eps.get_application_state_ptr(application_state::RPC_ADDRESS)) {
         auto state_addr = inet_address(addr_state->value());
         if (state_addr != node.addr) {
             on_internal_error(logger, format("endpoint_state_map::set: node {} has miamatching address={} in endpoint_state", node, state_addr));
         }
+    } else {
+        eps.add_application_state(application_state::RPC_ADDRESS, versioned_value::rpcaddress(node.addr));
     }
     auto it = _state_by_address.find(node.addr);
     if (it == _state_by_address.end()) {
