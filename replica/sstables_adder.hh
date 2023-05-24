30b6596b070ce1b5d19e2760bf558fb5b8c30e07
Benny Halevy <bhalevy@scylladb.com>
2023-05-24 14:22:27 +0300

    compaction_group: introduce compaction_group_sstables_adder
    Will be made exception safe in the following patches.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

---
+++ replica/sstables_adder.hh
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) 2014-present ScyllaDB
+ */
+
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#pragma once
+
+#include <vector>
+
+#include <seastar/util/bool_class.hh>
+
+#include "seastarx.hh"
+#include "row_cache.hh"
+#include "sstables/shared_sstable.hh"
+#include "sstables/sstable_set.hh"
+
+namespace replica {
+
+using is_main = bool_class<struct is_main_tag>;
+
+class compaction_group;
+
+class compaction_group_sstables_adder : public row_cache::external_updater_impl {
+protected:
+    compaction_group& _cg;
+    std::vector<sstables::shared_sstable> _sstables;
+    is_main _main;
+private:
+    lw_shared_ptr<sstables::sstable_set> _new_sstable_set;
+public:
+    compaction_group_sstables_adder(compaction_group& cg, std::vector<sstables::shared_sstable> sstables, is_main main);
+    // Guarantees strong exception safety
+    virtual future<> prepare() override;
+    // FIXME: need to provide strong exception guarantees.
+    virtual void execute() override;
+};
+
+} // namespace replica
