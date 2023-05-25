3efe29442cce519925bcb27f97a2190988511220
Benny Halevy <bhalevy@scylladb.com>
2023-05-25 14:26:49 +0300

    row_cache: abort on exteral_updater::execute errors
    Currently the cache updaters aren't exception safe
    yet they are intended to be.
    
    Instead of allowing exception to escape execute(),
    make it noexcept in the pure virtual external_updater_impl.
    Implementation that are able to provide strong exception
    guarantees in execute() should still throw nothing,
    but make the exception available via get_exception()
    so that it can be propagated from cache::update/invalidate.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- row_cache.cc
+++ row_cache.cc
@@ -1346,8 +1346,11 @@ future<> row_cache::do_update(row_cache::external_updater eu, row_cache::interna
         return get_units(_update_sem, 1);
     }).then([this, &eu, &iu] (auto permit) mutable {
       return eu.prepare().then([this, &eu, &iu, permit = std::move(permit)] () mutable {
-        eu.execute();
         [&] () noexcept {
+            // Any error from execute is considered fatal
+            // to enforce exception safety.
+            eu.execute();
+
             _prev_snapshot_pos = dht::ring_position::min();
             _prev_snapshot = std::exchange(_underlying, _snapshot_source());
             ++_underlying_phase;
