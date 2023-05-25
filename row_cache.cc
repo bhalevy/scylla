bf22a13a9878471425e99b0d92b30e73b780f4e4
Benny Halevy <bhalevy@scylladb.com>
2023-05-25 19:33:55 +0300

    row_cache: reindent do_update
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- row_cache.cc
+++ row_cache.cc
@@ -1342,27 +1342,26 @@ std::ostream& operator<<(std::ostream& out, row_cache& rc) {
 }
 
 future<> row_cache::do_update(row_cache::external_updater eu, row_cache::internal_updater iu) noexcept {
-  // FIXME: indentation
     auto permit = co_await get_units(_update_sem, 1);
     co_await eu.prepare();
-        [&] () noexcept {
-            // Any error from execute is considered fatal
-            // to enforce exception safety.
-            eu.execute();
+    [&] () noexcept {
+        // Any error from execute is considered fatal
+        // to enforce exception safety.
+        eu.execute();
 
-            _prev_snapshot_pos = dht::ring_position::min();
-            _prev_snapshot = std::exchange(_underlying, _snapshot_source());
-            ++_underlying_phase;
-        }();
-        co_await futurize_invoke([&iu] {
-            return iu();
-        }).then_wrapped([this, permit = std::move(permit)] (auto f) {
-            _prev_snapshot_pos = {};
-            _prev_snapshot = {};
-            if (f.failed()) {
-                clogger.warn("Failure during cache update: {}", f.get_exception());
-            }
-        });
+        _prev_snapshot_pos = dht::ring_position::min();
+        _prev_snapshot = std::exchange(_underlying, _snapshot_source());
+        ++_underlying_phase;
+    }();
+    co_await futurize_invoke([&iu] {
+        return iu();
+    }).then_wrapped([this, permit = std::move(permit)] (auto f) {
+        _prev_snapshot_pos = {};
+        _prev_snapshot = {};
+        if (f.failed()) {
+            clogger.warn("Failure during cache update: {}", f.get_exception());
+        }
+    });
 }
 
 std::ostream& operator<<(std::ostream& out, const cache_entry& e) {
