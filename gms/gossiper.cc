ebf4bc8f85cf0f7f3d8b52d26a12c78052254f61
Benny Halevy <bhalevy@scylladb.com>
2023-07-23 12:25:31 +0300

    gossiper: do_on_*notifications: mark as const
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- gms/gossiper.cc
+++ gms/gossiper.cc
@@ -1817,19 +1817,19 @@ future<> gossiper::apply_new_states(inet_address addr, endpoint_state& local_sta
     maybe_rethrow_exception(std::move(ep));
 }
 
-future<> gossiper::do_before_change_notifications(inet_address addr, const endpoint_state& ep_state, const application_state& ap_state, const versioned_value& new_value) {
+future<> gossiper::do_before_change_notifications(inet_address addr, const endpoint_state& ep_state, const application_state& ap_state, const versioned_value& new_value) const {
     co_await _subscribers.for_each([addr, ep_state, ap_state, new_value] (shared_ptr<i_endpoint_state_change_subscriber> subscriber) {
         return subscriber->before_change(addr, ep_state, ap_state, new_value);
     });
 }
 
-future<> gossiper::do_on_change_notifications(inet_address addr, const application_state& state, const versioned_value& value, permit_id pid) {
+future<> gossiper::do_on_change_notifications(inet_address addr, const application_state& state, const versioned_value& value, permit_id pid) const {
     co_await _subscribers.for_each([addr, state, value, pid] (shared_ptr<i_endpoint_state_change_subscriber> subscriber) {
         return subscriber->on_change(addr, state, value, pid);
     });
 }
 
-future<> gossiper::do_on_dead_notifications(inet_address addr, endpoint_state state, permit_id pid) {
+future<> gossiper::do_on_dead_notifications(inet_address addr, endpoint_state state, permit_id pid) const {
     co_await _subscribers.for_each([addr, state = std::move(state), pid] (shared_ptr<i_endpoint_state_change_subscriber> subscriber) {
         return subscriber->on_dead(addr, state, pid);
     });
