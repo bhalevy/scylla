b24b9f8657779734c7fd669171d770101debab1f
Benny Halevy <bhalevy@scylladb.com>
2023-11-14 14:00:17 +0200

    api: add /storage_service/flush
    For flushing all tables in the database.
    The advantage of this api is that `commitlog->force_new_active_segment`
    happens only once in `database::flush_all_tables` rather than
    once per keyspace (when `nodetool flush` translates to
    a sequence of `/storage_service/keyspace_flush` calls).
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- test/nodetool/test_flush.py
+++ test/nodetool/test_flush.py
@@ -8,7 +8,22 @@ from rest_api_mock import expected_request
 import utils
 
 
-def test_flush(nodetool):
+def test_flush_all_tables(nodetool, scylla_only):
+    nodetool("flush", expected_requests=[
+        expected_request("POST", "/storage_service/flush")
+    ])
+
+
+def test_flush_all_tables_jmx(nodetool, cassandra_only):
+    nodetool("flush", expected_requests=[
+            expected_request("GET", "/storage_service/keyspaces", multiple=expected_request.MULTIPLE,
+                            response=["ks1", "ks2"]),
+            expected_request("POST", "/storage_service/keyspace_flush/ks1"),
+            expected_request("POST", "/storage_service/keyspace_flush/ks2")
+    ])
+
+
+def test_flush_one_keyspace(nodetool):
     nodetool("flush", "ks1", expected_requests=[
         expected_request("GET", "/storage_service/keyspaces", response=["ks1", "ks2"]),
         expected_request("POST", "/storage_service/keyspace_flush/ks1")
