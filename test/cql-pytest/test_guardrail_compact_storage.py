1948ed3d07308613eefd27e493141357903d559d
Benny Halevy <bhalevy@scylladb.com>
2023-12-13 13:17:40 +0200

    config: add enable_create_table_with_compact_storage
    As discussed in
    https://github.com/scylladb/scylladb/issues/12263#issuecomment-1853576813,
    compact storage tables are deprecated.
    
    Yet, there's is nothing in the code that prevents users
    from creating such tables.
    
    This patch adds a live-updateable config option:
    `enable_create_table_with_compact_storage` that require users
    to opt-in in order to create new tables WITH COMPACT STORAGE.
    
    The option is currently set to `true` by default in db/config
    to reduce the churn to tests and to `false` in scylla.yaml,
    for new clusters.
    
    TODO: once regressions tests that use compact storage
    are converted to enable the option, change the default in
    db/config to false.
    
    A unit test was added to test/cql-pytest that
    checks that the respective cql query fails as expected
    with the default option or when it is explicitly set to `false`,
    and that the query succeeds when the option is set to `true`.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

---
+++ test/cql-pytest/test_guardrail_compact_storage.py
@@ -0,0 +1,32 @@
+import pytest
+from util import config_value_context, new_test_keyspace, new_test_table
+from cassandra_tests.porting import *
+
+# Tests for the enable_create_table_with_compact_storage guardrail.
+# Because this feature does not exist in Cassandra , *all* tests in this file are
+# Scylla-only. Let's mark them all scylla_only with an autouse fixture:
+@pytest.fixture(scope="function", autouse=True)
+def all_tests_are_scylla_only(scylla_only):
+    pass
+
+def test_create_table_with_compact_storage_default_config(cql, this_dc):
+    ks_defs = f"WITH REPLICATION = {{ 'class' : 'NetworkTopologyStrategy', '{this_dc}' : 1 }}"
+    with new_test_keyspace(cql, ks_defs) as test_keyspace:
+        # enable_create_table_with_compact_storage is still enabled in db/config
+        with new_test_table(cql, test_keyspace, schema="p int PRIMARY KEY, v int", extra="WITH COMPACT STORAGE") as test_table:
+            pass
+
+@pytest.mark.parametrize("config_value", [False, True])
+def test_create_table_with_compact_storage_config(cql, this_dc, config_value):
+    with config_value_context(cql, 'enable_create_table_with_compact_storage', str(config_value).lower()):
+        ks_defs = f"WITH REPLICATION = {{ 'class' : 'NetworkTopologyStrategy', '{this_dc}' : 1 }}"
+        with new_test_keyspace(cql, ks_defs) as test_keyspace:
+            try:
+                with new_test_table(cql, test_keyspace, schema="p int PRIMARY KEY, v int", extra="WITH COMPACT STORAGE") as test_table:
+                    if not config_value:
+                        pytest.fail
+            except InvalidRequest:
+                # expected to throw InvalidRequest only when
+                # enable_create_table_with_compact_storage=false
+                if config_value:
+                    pytest.fail
