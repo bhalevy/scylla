58d028aa36721cf0fc03b31dd13be62087249e21
Benny Halevy <bhalevy@scylladb.com>
2023-12-15 19:22:59 +0200

    storage_service: drop do_update_system_peers_table
    It is no longer used after previous patch.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- service/storage_service.hh
+++ service/storage_service.hh
@@ -470,10 +470,6 @@ private:
     data_values_map get_peer_info_for_update(inet_address endpoint);
     static data_values_map get_peer_info_for_update(inet_address endpoint, const gms::application_states_map& app_state_map);
 
-    template <typename T>
-    future<> update_table(gms::inet_address endpoint, sstring col, T value);
-    future<> do_update_system_peers_table(gms::inet_address endpoint, const application_state& state, const versioned_value& value);
-
     std::unordered_set<token> get_tokens_for(inet_address endpoint);
     std::optional<locator::endpoint_dc_rack> get_dc_rack_for(const gms::endpoint_state& ep_state);
     std::optional<locator::endpoint_dc_rack> get_dc_rack_for(inet_address endpoint);
