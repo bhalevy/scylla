a8c09d3595cfc67d477d8d3f7c09dab99d2d5f30
Benny Halevy <bhalevy@scylladb.com>
2024-04-04 13:14:36 +0300

    storage_service: merge_topology_snapshot: apply in background
    Since the cdc mutations aren't applied atomically
    anyway, apply them in parallel in the background
    with a parallelism limit of 128, as of today.
    Just do it in the for_each_split_mutation callback
    so we don't have to needlessly save a potentially long
    vector of frozen mutations.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -752,35 +752,38 @@ future<> storage_service::merge_topology_snapshot(raft_snapshot snp) {
     if (it != snp.mutations.end()) {
         auto s = _db.local().find_schema(db::system_keyspace::NAME, db::system_keyspace::CDC_GENERATIONS_V3);
 
-        // Split big mutations into smaller ones, prepare frozen_muts_to_apply
-        std::vector<frozen_mutation> frozen_muts_to_apply;
-        {
-            const auto max_size = _db.local().schema_commitlog()->max_record_size() / 2;
-            for (auto i = it; i != snp.mutations.end(); i++) {
-                const auto& m = *i;
-                auto mut = m.to_mutation(s);
-                if (m.representation().size() <= max_size) {
-                    frozen_muts_to_apply.emplace_back(freeze(mut));
-                    co_await coroutine::maybe_yield();
-                } else {
-                    co_await for_each_split_mutation(std::move(mut), max_size, [&] (mutation m) {
-                        frozen_muts_to_apply.emplace_back(freeze(m));
-                    });
-                }
-            }
-        }
-
+        // Split big mutations into smaller ones
         // Apply non-atomically so as not to hit the commitlog size limit.
         // The cdc_generations_v3 data is not used in any way until
         // it's referenced from the topology table.
         // By applying the cdc_generations_v3 mutations before topology mutations
         // we ensure that the lack of atomicity isn't a problem here.
-        //
-        // FIXME: we can apply the frozen mutations one at a time also above
-        // saving the need to keep a vector of all frozen mutations.
-        co_await max_concurrent_for_each(frozen_muts_to_apply, 128, [&] (const frozen_mutation& m) -> future<> {
-            return _db.local().apply(s, m, {}, db::commitlog::force_sync::yes, db::no_timeout);
-        });
+        {
+            semaphore sem(128);
+            const auto max_size = _db.local().schema_commitlog()->max_record_size() / 2;
+            future<> done = make_ready_future<>();
+            auto apply_mutation = [&] (frozen_mutation m) -> future<> {
+                auto units = get_units(sem, 1);
+                co_await _db.local().apply(s, m, {}, db::commitlog::force_sync::yes, db::no_timeout);
+            };
+            auto apply_in_background = [&] (frozen_mutation m) {
+                done = done.then([&, m = std::move(m)] () mutable {
+                    return apply_mutation(std::move(m));
+                });
+            };
+            for (auto i = it; i != snp.mutations.end(); i++) {
+                const auto& m = *i;
+                auto mut = m.to_mutation(s);
+                if (m.representation().size() <= max_size) {
+                    apply_in_background(freeze(mut));
+                } else {
+                    co_await for_each_split_mutation(std::move(mut), max_size, [&] (mutation m) {
+                        apply_in_background(freeze(m));
+                    });
+                }
+            }
+            co_await std::move(done);
+        }
     }
 
     // Apply system.topology and system.topology_requests mutations atomically
