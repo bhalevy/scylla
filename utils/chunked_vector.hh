31ff25aee33f2e9b3acd94e8d46cd135a7039aab
Benny Halevy <bhalevy@scylladb.com>
2024-05-09 18:58:32 +0300

    utils: chunked_vector: optimize for trivially_copyable types
    Use std::uninitialized_{copy,move} and std::destroy
    that have optimizations for trivially copyable and
    trivially moveable types.
    In those cases, memory can be copied onto the uninitialized
    memory, rather than invoking the respective copy/move constructors,
    one item at a time.
    
    perf-simple-query results:
    ```
    base: median 95954.90 tps ( 63.1 allocs/op,   0.0 logallocs/op,  14.1 tasks/op,   42312 insns/op,        0 errors)
    post: median 97530.65 tps ( 63.1 allocs/op,   0.0 logallocs/op,  14.1 tasks/op,   42331 insns/op,        0 errors)
    ```
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- utils/chunked_vector.hh
+++ utils/chunked_vector.hh
@@ -325,8 +325,16 @@ size_t chunked_vector<T, max_contiguous_allocation>::external_memory_usage() con
 template <typename T, size_t max_contiguous_allocation>
 chunked_vector<T, max_contiguous_allocation>::chunked_vector(const chunked_vector& x)
         : chunked_vector() {
-    reserve(x.size());
-    std::copy(x.begin(), x.end(), std::back_inserter(*this));
+    auto size = x.size();
+    reserve(size);
+    _size = size;
+    for (size_t i = 0; size; ++i) {
+        const T* src = x._chunks[i].get();
+        T* dst = _chunks[i].get();
+        auto n = std::min(size, max_chunk_capacity());
+        std::uninitialized_copy_n(src, n, dst);
+        size -= n;
+    }
 }
 
 template <typename T, size_t max_contiguous_allocation>
@@ -378,8 +386,13 @@ chunked_vector<T, max_contiguous_allocation>::operator=(chunked_vector&& x) noex
 template <typename T, size_t max_contiguous_allocation>
 chunked_vector<T, max_contiguous_allocation>::~chunked_vector() {
     if constexpr (!std::is_trivially_destructible_v<T>) {
-        for (auto i = size_t(0); i != _size; ++i) {
-            addr(i)->~T();
+        size_t i;
+        size_t full_chunks = _size / max_chunk_capacity();
+        for (i = 0; i < full_chunks; ++i) {
+            std::destroy_n(_chunks[i].get(), max_chunk_capacity());
+        }
+        if (auto remainder = _size % max_chunk_capacity()) {
+            std::destroy_n(_chunks[i].get(), remainder);
         }
     }
 }
@@ -397,12 +410,8 @@ chunked_vector<T, max_contiguous_allocation>::new_chunk(size_t n) {
 template <typename T, size_t max_contiguous_allocation>
 void
 chunked_vector<T, max_contiguous_allocation>::migrate(T* begin, T* end, T* result) {
-    while (begin != end) {
-        new (result) T(std::move(*begin));
-        begin->~T();
-        ++begin;
-        ++result;
-    }
+    std::uninitialized_move(begin, end, result);
+    std::destroy(begin, end);
 }
 
 template <typename T, size_t max_contiguous_allocation>
