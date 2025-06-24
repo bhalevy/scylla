513fff41512818e286ac4814f8840e7d7309532e
Benny Halevy <bhalevy@scylladb.com>
2025-06-24 08:04:44 +0300

    token_metadata: keep a reference to shared_token_metadata
    To be used by a following patch to gently clean and destroy
    the token_data_impl in the background.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- locator/token_metadata.cc
+++ locator/token_metadata.cc
@@ -94,9 +94,12 @@ public:
     token_metadata_impl(shallow_copy, const token_metadata_impl& o) noexcept
         : _topology(topology::shallow_copy{}, topology::config{})
     {}
-    token_metadata_impl(token_metadata::config cfg) noexcept : _topology(std::move(cfg.topo_cfg)) {};
+    token_metadata_impl(token_metadata::config cfg) noexcept
+        : _topology(std::move(cfg.topo_cfg))
+    {};
     token_metadata_impl(const token_metadata_impl&) = delete; // it's too huge for direct copy, use clone_async()
     token_metadata_impl(token_metadata_impl&&) noexcept = default;
+
     const std::vector<token>& sorted_tokens() const;
     future<> update_normal_tokens(std::unordered_set<token> tokens, host_id endpoint);
     const token& first_token(const token& start) const;
@@ -834,8 +837,9 @@ token_metadata::token_metadata(std::unique_ptr<token_metadata_impl> impl)
 {
 }
 
-token_metadata::token_metadata(config cfg)
-        : _impl(std::make_unique<token_metadata_impl>(cfg))
+token_metadata::token_metadata(shared_token_metadata& stm, config cfg)
+        : _shared_token_metadata(&stm)
+        , _impl(std::make_unique<token_metadata_impl>(std::move(cfg)))
 {
 }
 
@@ -844,6 +848,10 @@ token_metadata::~token_metadata() = default;
 token_metadata::token_metadata(token_metadata&&) noexcept = default;
 
 token_metadata& token_metadata::token_metadata::operator=(token_metadata&&) noexcept = default;
+
+void token_metadata::set_shared_token_metadata(shared_token_metadata& stm) {
+    _shared_token_metadata = &stm;
+}
 
 const std::vector<token>&
 token_metadata::sorted_tokens() const {
@@ -1154,6 +1162,7 @@ void shared_token_metadata::set(mutable_token_metadata_ptr tmptr) noexcept {
         _stale_versions_in_use = _versions_barrier.advance_and_await();
     }
 
+    tmptr->set_shared_token_metadata(*this);
     _shared = std::move(tmptr);
     _shared->set_version_tracker(new_tracker(_shared->get_version()));
 
