ef7bc5e2dd1c578ffb0ff925da17b1f5510a8b4e
Benny Halevy <bhalevy@scylladb.com>
2025-11-25 09:39:35 +0200

    sstable: add _mutate_sem to serialize link/move with components rewrite
    We currently have races, like between moving an sstable from staging
    using change_state, or when taking a snapshot, to e.g.
    rewrite_statistics that replaces one of the sstable component files
    when called, for example, from update_repaired_at by incremental repair.
    
    Use a semaphore as a mutex to serialize those functions.
    Note that there is no need for rwlock since the operations
    are rare and read-only operations like snapshot don't
    need to run in parallel.
    
    Fixes #25919
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- sstables/sstables.hh
+++ sstables/sstables.hh
@@ -630,6 +630,10 @@ private:
     bool _unlinked{false};
 
     components_digests _components_digests;
+
+    // The mutate semaphore is used to serialize operations like rewrite_statistics
+    // with linking or moving the sstable between directories.
+    mutable named_semaphore _mutate_sem{1, named_semaphore_exception_factory{"sstable mutate"}};
 public:
     bool has_component(component_type f) const;
     sstables_manager& manager() { return _manager; }
@@ -719,6 +723,7 @@ private:
     void write_statistics();
     // Rewrite statistics component by creating a temporary Statistics and
     // renaming it into place of existing one.
+    // This function must run inside a seastar thread since it calls `write`
     void rewrite_statistics();
     // Validate metadata that's used to optimize reads when user specifies
     // a clustering key range. If this specific metadata is incorrect, then
