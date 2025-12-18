5821f1c962b0e1dd339b7f25be91a55f1cc084db
Benny Halevy <bhalevy@scylladb.com>
2025-12-18 09:39:16 +0200

    test: database_test: add test_flush_empty_table_waits_on_outstanding_flush
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

--- test/boost/database_test.cc
+++ test/boost/database_test.cc
@@ -1958,4 +1958,27 @@ SEASTAR_TEST_CASE(test_tombstone_gc_state_gc_mode) {
     return make_ready_future<>();
 }
 
+SEASTAR_TEST_CASE(test_flush_empty_table_waits_on_outstanding_flush) {
+    return do_with_some_data({"cf"}, [] (cql_test_env& e) -> future<> {
+        auto& cf = e.local_db().find_column_family("ks", "cf");
+
+        BOOST_REQUIRE(cf.needs_flush());
+        auto flushed = cf.flush();
+        if (flushed.available()) {
+            testlog.error("Table flush completed too early");
+            BOOST_REQUIRE(!flushed.available());
+        }
+
+        // Now flush again when the active memtable is empty.
+        // Expect that this waits on the ongoing flush
+        BOOST_REQUIRE(cf.needs_flush());
+        co_await cf.flush();
+        if (cf.needs_flush()) {
+            testlog.error("Table is not expected to need flush after flushing empty active memtable");
+            BOOST_REQUIRE(!cf.needs_flush());
+        }
+        co_await std::move(flushed);
+    });
+}
+
 BOOST_AUTO_TEST_SUITE_END()
