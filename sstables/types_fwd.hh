90455a94b3a11cf99b3c3d404a47160daa43a5f3
Benny Halevy <bhalevy@scylladb.com>
2026-07-14 11:37:04 +0300

    sstables: introduce enum class update_sstable_id
    Prepare for having tri-state values where the `maybe` policy
    would allow e.g. link_with_rewritten_component to decided whether
    to update the sstable_id based on the sstable storage type.

--- sstables/types_fwd.hh
+++ sstables/types_fwd.hh
@@ -17,4 +17,10 @@ namespace sstables {
 using run_id = utils::tagged_uuid<struct run_id_tag>;
 using integrity_check = bool_class<class integrity_check_tag>;
 
+enum class update_sstable_id {
+    no,
+    yes,
+    // FIXME: `maybe` lets the lower level decide automatically based on the sstable storage type.
+};
+
 } // namespace sstables
